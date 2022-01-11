3a181ebe6cb1e15137e4ba5e39040f45521b9cbc
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-01-11 12:38:48 +0530

    replaced the broken icons with a generic icon; (#2)
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -129,21 +129,21 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "src/assets/img/icon-128.png",
+          from: "src/assets/img/icon.png",
           to: path.join(__dirname, "build"),
           force: true,
         },
       ],
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "src/assets/img/icon-34.png",
-          to: path.join(__dirname, "build"),
-          force: true,
-        },
-      ],
-    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: "src/assets/img/icon-34.png",
+    //       to: path.join(__dirname, "build"),
+    //       force: true,
+    //     },
+    //   ],
+    // }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "pages", "Options", "index.html"),
       filename: "options.html",
