784dde2aed76dd61dde77de813ad9434a70a3b16
Mukul Mehta <hello@mukul-mehta.in>
2022-01-19 18:05:50 +0530

    Add login on the browser extension (#6)
    https://github.com/workduck-io/browser-extension-frontend/pull/6

---
+++ src/Components/Auth.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useAuthentication, useAuthStore } from '../Hooks/useAuth'
+
+import { LoginFormData } from '../Types/Auth'
+
+export const Login = () => {
+  const [loginResult, setLoginResult] = useState('')
+  const { login } = useAuthentication()
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm<LoginFormData>()
+
+  const onSubmit = async (data: LoginFormData): Promise<void> => {
+    let temp: any
+    await login(data.email, data.password, true)
+      .then((s) => {
+        if (s.v === 'Incorrect username or password.') {
+          console.log('Invalid Username or Password')
+          setLoginResult('Invalid Username or Password')
+        }
+        temp = s.data
+      })
+      .catch((e) => {
+        console.error('Error occured: ', e)
+        temp = 'Error Occurred'
+      })
+    setLoginResult(JSON.stringify(temp))
+  }
+
+  return (
+    <>
+      <h3>Login</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('email')} />
+        <input type="password" {...register('password')} />
+
+        <button type="submit">Submit</button>
+      </form>
+      <p>{loginResult}</p>
+    </>
+  )
+}
+
+export const Logout = () => {
+  const { logout } = useAuthentication()
+
+  const onLogout = () => {
+    logout()
+  }
+
+  return (
+    <>
+      <button onClick={onLogout}>Logout</button>
+    </>
+  )
+}
+
+const Auth = () => {
+  const authenticated = useAuthStore((store) => store.authenticated)
+  console.log('Is Authenticated: ', authenticated)
+
+  return (
+    <>
+      {authenticated ? (
+        <>
+          <h3>LMAO BRUH YOU ALREADY LOGGED IN</h3>
+          <Logout />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
+    </>
+  )
+}
+
+export default Auth
---
+++ src/Hooks/useAuth.ts
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import { useAuth, client } from '@workduck-io/dwindle'
+import { UserCred } from '@workduck-io/dwindle/lib/esm/AuthStore/useAuthStore'
+import create, { State } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+import { apiURLs } from '../routes'
+import { RegisterFormData } from '../Types/Auth'
+
+interface UserDetails {
+  email: string
+}
+
+interface WorkspaceDetails {
+  name: string
+  id: string
+}
+
+interface AuthStoreState extends State {
+  authenticated: boolean
+  registered: boolean
+  userDetails: undefined | UserDetails
+  workspaceDetails: undefined | WorkspaceDetails
+  setAuthenticated: (userDetails: UserDetails, workspaceDetails: WorkspaceDetails) => void
+  setUnAuthenticated: () => void
+  setRegistered: (val: boolean) => void
+  getWorkspaceId: () => string | undefined
+}
+
+export const useAuthStore = create<AuthStoreState>(
+  persist(
+    (set, get) => ({
+      authenticated: false,
+      registered: false,
+      userDetails: undefined,
+      workspaceDetails: undefined,
+      setAuthenticated: (userDetails, workspaceDetails) =>
+        set({ authenticated: true, userDetails, workspaceDetails, registered: false }),
+      // setAuthenticatedUserDetails: (userDetails: UserDetails) => set({ authenticated: true, userDetails }),
+      setUnAuthenticated: () => set({ authenticated: false, userDetails: undefined, workspaceDetails: undefined }),
+      setRegistered: (val) => set({ registered: val }),
+      getWorkspaceId: () => {
+        const workspaceDetails = get().workspaceDetails
+        if (workspaceDetails) {
+          return workspaceDetails.id
+        }
+        return undefined
+      }
+    }),
+    { name: 'auth-mex-extension' }
+  )
+)
+
+export const useAuthentication = () => {
+  const setAuthenticated = useAuthStore((store) => store.setAuthenticated)
+  const setUnAuthenticated = useAuthStore((store) => store.setUnAuthenticated)
+  const { signIn, signOut, signUp, verifySignUp } = useAuth()
+
+  const setRegistered = useAuthStore((store) => store.setRegistered)
+  const [sensitiveData, setSensitiveData] = useState<RegisterFormData | undefined>()
+
+  const login = async (
+    email: string,
+    password: string,
+    getWorkspace = false
+  ): Promise<{ data: UserCred; v: string }> => {
+    let data: any // eslint-disable-line @typescript-eslint/no-explicit-any
+    const v = await signIn(email, password)
+      .then((d) => {
+        data = d
+        return 'success'
+      })
+      .catch((e) => {
+        console.error({ e })
+        return e.toString() as string
+      })
+
+    if (getWorkspace && data !== undefined) {
+      await client
+        .get(apiURLs.getUserRecords(data.userId))
+        .then((d: any) => {
+          const userDetails = { email }
+          const workspaceDetails = { id: d.data.group, name: 'WORKSPACE_NAME' }
+
+          setAuthenticated(userDetails, workspaceDetails)
+        })
+        .then()
+        .catch((e) => {
+          console.error({ e })
+          return e.toString() as string
+        })
+    }
+    return { data, v }
+  }
+
+  const logout = () => {
+    signOut().then(() => {
+      setUnAuthenticated()
+    })
+  }
+
+  return { login, logout }
+}
