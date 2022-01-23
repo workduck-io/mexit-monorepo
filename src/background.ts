cab8d2e6e3a9bf8abb7cf85f2d90a77b87be8fc4
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-01-24 01:39:01 +0530

    Closed sputlit on escape or click outside (#11)
    https://github.com/workduck-io/browser-extension-frontend/pull/11
    * closed sputlit on escape;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * closed sputlit on click outside of it;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,20 @@
 // Anything here runs in the background
 
+import { getCurrentTab } from './Utils/tabInfo'
+
 chrome.commands.onCommand.addListener((command) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { type: 'sputlit' })
   })
+})
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.request) {
+    case 'close-sputlit':
+      getCurrentTab().then((response) => {
+        chrome.tabs.sendMessage(response.id, { type: 'close-sputlit' })
+      })
+  }
 })
 
 export {}
