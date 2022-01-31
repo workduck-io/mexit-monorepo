4316ccaf2592fb569dbc54551fae9cf3a4f9b137
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-02-01 03:01:18 +0530

    Sahil wd 846 highlight removal (#25)
    https://github.com/workduck-io/mexit-extension/pull/25
    * added highlight removal tooltip;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * added TODO;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * changed render method of tooltip;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * stored highlighter id, highlight removal added, inbuilt range serializer used;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * persist highlight is back;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

---
+++ src/Components/Tooltip.tsx
@@ -0,0 +1,56 @@
+import React, { useContext, useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+import { useContentStore } from '../Hooks/useContentStore'
+import styled from 'styled-components'
+import Highlighter from 'web-highlighter'
+
+const Container = styled.div<{ top: number; left: number; showTooltip: boolean }>`
+  position: absolute;
+  display: ${(props) => (props.showTooltip ? 'flex' : 'none')};
+  margin: -3rem 0 0 0;
+  cursor: pointer;
+
+  background: #fff;
+  box-shadow: 0 2px 2px 0 rgb(39 43 49 / 10%);
+  padding: 0.25rem;
+  border: solid 1px #ccc;
+  border-radius: 5px;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+
+  /* Done because the root is removed before the component is umounted, hence it still stays there */
+  &:empty {
+    padding: 0;
+  }
+`
+
+function Tooltip({ id, coordinates }: { id: string; coordinates: DOMRect }) {
+  const [showTooltip, setShowTooltip] = useState(true)
+  const removeContent = useContentStore((store) => store.removeContent)
+  const highligter = new Highlighter()
+
+  const handleDelete = () => {
+    highligter.remove(id)
+    setShowTooltip(false)
+    removeContent(window.location.href, id)
+  }
+
+  useEffect(() => {
+    setShowTooltip(true)
+  }, [id])
+
+  // TODO: remove this component somehow.
+  return (
+    <Container
+      top={window.scrollY + coordinates.top}
+      left={window.scrollX + coordinates.left}
+      showTooltip={showTooltip}
+    >
+      <div onClick={handleDelete}>
+        <img src={chrome.runtime.getURL('/Assets/trash.svg')} />
+      </div>
+    </Container>
+  )
+}
+
+export default Tooltip
--- src/Hooks/useContentStore.ts
+++ src/Hooks/useContentStore.ts
@@ -7,7 +7,7 @@ import storageAdapter from '../Utils/chromeStorageAdapter'
 export interface ContentStoreState extends State {
   contents: Contents
   getContent: (url: string) => Content[]
-  removeContent: (url: string) => void
+  removeContent: (url: string, highlighterId: string) => void
   setContent: (url: string, content: NodeEditorContent, saveableRange: Partial<HighlightSource>, id: string) => void
 }
 
@@ -27,6 +27,7 @@ export const useContentStore = create<ContentStoreState>(
             ...urlContent,
             {
               id: id,
+              highlighterId: saveableRange.id,
               content: content,
               range: saveableRange
             }
@@ -40,9 +41,17 @@ export const useContentStore = create<ContentStoreState>(
         console.log('Getting Content for URL: ', url)
         return get().contents[url]
       },
-      removeContent: (url) => {
-        const oldContent = get()?.contents
-        delete oldContent[url]
+      removeContent: (url, highlighterId) => {
+        const oldContent = get()?.contents[url]
+        const idx = oldContent.map((i) => i.highlighterId).indexOf(highlighterId)
+
+        oldContent.splice(idx, 1)
+        set({
+          contents: {
+            ...get().contents,
+            [url]: oldContent
+          }
+        })
       }
     }),
     {
