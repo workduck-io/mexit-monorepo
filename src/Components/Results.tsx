c36eac61e75e085f40b921fe706d8568b2630b0d
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-02-02 20:57:46 +0530

    Virtualized action list rendering (#30)
    * deps: added react-virtual package;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * updated font assests; commented out font family due to FOUT;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * virtualized the rendering of actions list;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * fixed margins and paddings a bit;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * scroll behaviour added;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- src/Components/Results.tsx
+++ src/Components/Results.tsx
@@ -1,90 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import usePointerMovedSinceMount from '../Hooks/usePointerMovedSinceMount'
 import styled, { css } from 'styled-components'
 import { ActionType, MexitAction } from '../Types/Actions'
 import { actionExec } from '../Utils/actionExec'
+import { useVirtual } from 'react-virtual'
+import Action from './Action'
+import { ReactEditor } from 'slate-react'
 
-const List = styled.div`
+const List = styled.div<{ height: number }>`
   width: 100%;
-  overflow: overlay;
+  overflow: auto;
+  position: relative;
+  scroll-behavior: smooth;
 
+  height: ${(props) => props.height}px;
   max-height: 400px;
 `
 
-const ListItem = styled.div<{ active: boolean }>`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-
-  height: 60px;
-  padding: 0 0 0 1rem;
-  color: #111;
-  border-left: 2px solid transparent;
+const ListItem = styled.div<{ start: number }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateY(${(props) => props.start}px);
+  width: 100%;
 
   &:hover {
     cursor: pointer;
   }
-
-  ${(props) =>
-    props.active &&
-    css`
-       {
-        background-color: rgba(0, 0, 0, 0.05);
-        border-left: 2px solid rgb(28, 28, 29);
-      }
-    `}
 `
 
-const Action = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
-const ActionIcon = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: 0.75rem;
-
-  img {
-    height: 24px;
-    aspect-ratio: 1/1;
-  }
-`
-
-const ActionDesc = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  h3 {
-    font-size: 1.1rem;
-    font-weight: 400;
-    margin: 0;
-  }
-
-  p {
-    font-size: 0.85rem;
-    margin: 0.25rem 0 0.5rem 0;
-  }
-`
 const Subtitle = styled.div`
   padding: 0.5rem 1rem;
   text-transform: uppercase;
   font-size: 0.75rem;
   opacity: 0.5;
-`
-
-const Shortcut = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 1rem;
-`
-
-const Key = styled.span`
-  background: rgba(0, 0, 0, 0.1);
-  border-radius: 4px;
-  margin: 0.25rem;
-  padding: 0.25rem;
 `
 
 function Results({
@@ -100,10 +49,28 @@ function Results({
   selectedAction: MexitAction
   setSelectedAction: (action: MexitAction) => void
 }) {
+  const parentRef = useRef(null)
   const [activeIndex, setActiveIndex] = useState(0)
   const [first, setFirst] = useState(false)
   const [showResults, setShowResults] = useState(true)
   const pointerMoved = usePointerMovedSinceMount()
+
+  const rowVirtualizer = useVirtual({
+    size: actions.length,
+    parentRef
+  })
+
+  // destructuring here to prevent linter warning to pass
+  // entire rowVirtualizer in the dependencies array.
+  const { scrollToIndex } = rowVirtualizer
+  React.useEffect(() => {
+    scrollToIndex(activeIndex, {
+      // ensure that if the first item in the list is a group
+      // name and we are focused on the second item, to not
+      // scroll past that group, hiding it.
+      align: activeIndex <= 1 ? 'end' : 'auto'
+    })
+  }, [activeIndex, scrollToIndex])
 
   // TODO: lesser re-renders on using ref for active item, see KBarResults.tsx
   useEffect(() => {
@@ -184,33 +151,25 @@ function Results({
   }
 
   return (
-    // Better to use something like react-virtual to make scrolling and rendering a big list of actions better
     <>
       {showResults && (
         <>
           <Subtitle>Navigation</Subtitle>
-          <List>
-            {actions.map((item, id) => (
-              <ListItem
-                key={id}
-                active={activeIndex === id}
-                onPointerMove={() => pointerMoved && setActiveIndex(id)}
-                onClick={() => handleClick(id)}
-              >
-                <Action>
-                  <ActionIcon>
-                    {item.data.icon && <img src={chrome.runtime.getURL(`/Assets/${item.data.icon}`)} />}
-                  </ActionIcon>
-                  <ActionDesc>
-                    <h3> {item.title}</h3>
-                    {item.description && <p>{item.description}</p>}
-                  </ActionDesc>
-                </Action>
-                <Shortcut>
-                  {item.shortcut && item.shortcut.map((shortcutKey, id) => <Key key={id}>{shortcutKey}</Key>)}
-                </Shortcut>
-              </ListItem>
-            ))}
+          <List ref={parentRef} height={rowVirtualizer.totalSize}>
+            {rowVirtualizer.virtualItems.map((virtualRow) => {
+              const item = actions[virtualRow.index]
+              const handlers = {
+                onPointerMove: () => pointerMoved && setActiveIndex(virtualRow.index),
+                onClick: () => handleClick(virtualRow.index)
+              }
+              const active = virtualRow.index === activeIndex
+
+              return (
+                <ListItem key={virtualRow.index} ref={virtualRow.measureRef} start={virtualRow.start} {...handlers}>
+                  <Action action={item} active={active} />
+                </ListItem>
+              )
+            })}
           </List>
         </>
       )}
