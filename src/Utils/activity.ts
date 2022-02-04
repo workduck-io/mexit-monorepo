fd0513783927afaf1ce0e554bf523a5404f1608f
Mukul Mehta <hello@mukul-mehta.in>
2022-02-04 08:56:41 +0530

    Add Flexsearch to Webapp (#7)
    https://github.com/workduck-io/mexit-webapp/pull/7
    
    * define interfaces for node and blocks and rewrite search parser
    
    * Search store with block level indexing
    
    * Add sample nodes in new format
    
    * Add example of flexsearch in MainArea component
    
    * Rename BlockSearchData to BlockIndexData

--- src/Utils/activity.ts
+++ src/Utils/activity.ts
@@ -1,106 +1,114 @@
-export const activities = {
-  count: '20',
-  blocks: [
+import { Node } from '../Types/Data'
+
+export const activityNode: Node = {
+  id: '12345',
+  content: [
     {
-      id: 'LINK_QC_1234567',
-      createdBy: 'Sample User ID',
-      workspace: '12345',
-      long: 'https://developer.chrome.com/docs/extensions/reference/tabs/',
-      short: 'chrome-storage',
-      namespace: 'WORKSPACE_NAME',
-      metadata: {
-        metaTags: [
-          {
-            name: 'og:site_name',
-            value: 'Chrome Developers'
-          },
-          {
-            name: 'og:title',
-            value: 'chrome.tabs - Chrome Developers'
-          },
-          {
-            name: 'og:type',
-            value: 'website'
-          },
-          {
-            name: 'og:url',
-            value: 'https://developer.chrome.com/docs/extensions/reference/tabs/'
-          },
-          {
-            name: 'title',
-            value: 'chrome.tabs - Chrome Developers'
-          }
-        ],
-        userTags: [
-          {
-            id: 'TAG_pBsS4xcmccdE0UMjYIQDV',
-            text: 'dev'
-          },
-          {
-            id: 'TAG_AHPNZ7hrDN9XnFboe5kSV',
-            text: 'work'
-          },
-          {
-            id: 'TAG_pZK3c4ya0c9ylxc-sYEeI',
-            text: 'stuff'
-          }
-        ]
-      },
-      shortenedURL: 'https://url.workduck.io/link/WORKSPACE_NAME/chrome-storage'
+      type: 'p',
+      id: 'TEMP_dYEwUeJ9EHgwMadfJ3diJ',
+      nodeUID: '12345',
+      children: [
+        {
+          type: 'p',
+          id: 'TEMP_nmgjRAdzEyAErqbRLzykG',
+          text: 'This is block 1, and here is some italics '
+        },
+        {
+          type: 'p',
+          id: 'TEMP_NJmxpLWRQJMFG3mmhz73q',
+          italic: true,
+          text: 'yo yo yo. '
+        },
+        {
+          type: 'p',
+          id: 'TEMP_gGGVNrGpbGLtfCHYQdewV',
+          text: 'This is some non-italics again'
+        }
+      ]
     },
     {
-      id: 'CONTENT_QC_OaZGP3xBcuKLAOoh8pJ4O',
-      long: 'https://rakaar.github.io/posts/2022-01-23-dystopian-december',
-      createdBy: 'Sample User ID',
-      workspace: '12345',
-      content: [
+      type: 'p',
+      id: 'TEMP_yiEwBEVpJLw9D6KD6mzLW',
+      nodeUID: '12345',
+      children: [
         {
-          text: 'Orwell depicted methods of a power hungry totalitarian regime and human conditions under such a regime. It is a dark book, compared to the rest. While Brave New World by Aldous Huxley felt more of a utopian world with the ideas like '
-        },
-        {
-          text: 'everyone belongs to everyone else'
-        },
-        {
-          text: ' and '
-        },
-        {
-          text: 'soma'
-        },
-        {
-          text: '. The book conveys the idea that '
+          type: 'p',
+          id: 'TEMP_Qe7hFBgxrHbxpnj7HdNjJ',
+          text: ''
         }
-      ],
-      range: {
-        startMeta: {
-          parentTagName: 'P',
-          parentIndex: 4,
-          textOffset: 15
-        },
-        endMeta: {
-          parentTagName: 'EM',
-          parentIndex: 4,
-          textOffset: 64
-        },
-        text: 'Orwell depicted methods of a power hungry totalitarian regime and human conditions under such a regime. It is a dark book, compared to the rest. While Brave New World by Aldous Huxley felt more of a utopian world with the ideas like everyone belongs to everyone else and soma. The book conveys the idea that '
-      },
-      metadata: {
-        metaTags: [
-          {
-            name: 'og:title',
-            value: 'Dystopian December | Raghavendra Kaushik'
-          }
-        ],
-        userTags: [
-          {
-            id: 'TAG_pBsS4xcmccdE0UMjYIQDV',
-            text: 'rakaar'
-          },
-          {
-            id: 'TAG_AHPNZ7hrDN9XnFboe5kSV',
-            text: 'reading'
-          }
-        ]
-      }
+      ]
+    },
+    {
+      type: 'p',
+      id: 'TEMP_U7tQFwhEWK7P7FaC7WT9F',
+      nodeUID: '12345',
+      children: [
+        {
+          type: 'p',
+          id: 'TEMP_yCcxDyYmfh9yhHcDWKEyj',
+          text: 'This is another'
+        }
+      ]
+    },
+    {
+      type: 'p',
+      id: 'TEMP_4fLEyGKFjCBPt4ArVHqDR',
+      nodeUID: '12345',
+      children: [
+        {
+          type: 'p',
+          id: 'TEMP_Y7nAdKB4FYFqziFYiRE8G',
+          text: ''
+        }
+      ]
+    },
+    {
+      type: 'h1',
+      id: 'TEMP_feKePQYDViLhHq3DjtRqx',
+      nodeUID: '12345',
+      children: [
+        {
+          type: 'p',
+          id: 'TEMP_TRfUdkPLGrkdcp9Xti6D7',
+          text: 'This is yet another block'
+        }
+      ]
     }
   ]
 }
+
+export const exampleNode: Node = {
+  id: '69420',
+  content: [
+    {
+      type: 'p',
+      id: 'CONTENT_QC_OaZGP3xBcuKLAOoh8pJ4O',
+      nodeUID: '69420',
+      children: [
+        {
+          type: 'p',
+          text: 'Orwell depicted methods of a power hungry totalitarian regime and human conditions under such a regime. It is a dark book, compared to the rest. While Brave New World by Aldous Huxley felt more of a utopian world with the ideas like '
+        },
+        {
+          type: 'p',
+          text: 'everyone belongs to everyone else'
+        },
+        {
+          type: 'p',
+          text: ' and ',
+          italics: true
+        },
+        {
+          type: 'p',
+          text: 'soma'
+        },
+        {
+          type: 'p',
+          text: '. The book conveys this very idea.'
+        }
+      ]
+    }
+  ]
+}
+
+export const nodes: Node[] = [activityNode, exampleNode]
