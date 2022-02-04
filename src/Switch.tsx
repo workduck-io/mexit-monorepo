a1b36bd9f2cbd406e5caa6f3cb5e1a455b8f4df9
Mukul Mehta <hello@mukul-mehta.in>
2022-02-04 22:29:06 +0530

    More Sentry stuff because why not? (#9)
    https://github.com/workduck-io/mexit-webapp/pull/9
    
    * Add tracing to sentry with custom instrumentation for react-router v6
    
    * Update sentry DSN
    
    * Add error boundary to main area

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { Navigate, Route, RouteProps, Routes } from 'react-router-dom'
 import { useAuth } from '@workduck-io/dwindle'
+import useRoutingInstrumentation from 'react-router-v6-instrumentation'
+import { init as SentryInit } from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 
 import MainArea from './Views/MainArea'
 import { useAuthStore } from './Hooks/useAuth'
@@ -22,6 +25,7 @@ const AuthRoute = ({ children }) => {
 
 const Switch = () => {
   const { initCognito } = useAuth()
+  const routingInstrumentation = useRoutingInstrumentation()
 
   useEffect(() => {
     const userAuthenticatedEmail = initCognito({
@@ -31,6 +35,20 @@ const Switch = () => {
 
     console.log('User Authenticated Email: ', userAuthenticatedEmail)
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    const browserTracing = new BrowserTracing({
+      routingInstrumentation
+    })
+
+    SentryInit({
+      dsn: 'https://53b95f54a627459c8d0e74b9bef36381@o1135527.ingest.sentry.io/6184488',
+      tracesSampleRate: 1.0,
+      integrations: [browserTracing]
+    })
+
+    console.log('Sentry Initialized!')
+  }, [routingInstrumentation])
 
   return (
     <Routes>
