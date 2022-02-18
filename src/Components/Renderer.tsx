aedfb89d57f692a8ab31f583e012211a4afbfa06
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-02-18 16:59:08 +0530

    WIP moving render type actions to iframe;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- src/Components/Renderer.tsx
+++ src/Components/Renderer.tsx
@@ -1,32 +1,14 @@
 import React, { Suspense } from 'react'
+import styled from 'styled-components'
 
-import AliasWrapper from './Actions/AliasWrapper'
-import { CreateTabCapture, ShowTabCaptures } from './TabCaptures'
-import ColourPicker from './Actions/ColourPicker'
-import UnixEpochConverter from './Actions/UnixEpoch'
-import CorporateBS from './Actions/CorpBS'
-import LibreTranslate from './Actions/LibreTranslate'
-import CurrencyConverter from './Actions/CurrencyConverter'
+const Iframe = styled.iframe`
+  border: none;
+  margin: 0;
+  padding: 0;
+`
 
-const Renderer = ({ componentName, componentProps }: { componentName: string; componentProps: any }) => {
-  const components = {
-    AliasWrapper: AliasWrapper,
-    CreateTabCapture: CreateTabCapture,
-    ShowTabCaptures: ShowTabCaptures,
-    ColourPicker: ColourPicker,
-    UnixEpochConverter: UnixEpochConverter,
-    CorporateBS: CorporateBS,
-    LibreTranslate: LibreTranslate,
-    CurrencyConverter: CurrencyConverter
-  }
-
-  const ActionComponent = components[componentName]
-
-  return (
-    <Suspense fallback={<></>}>
-      <ActionComponent {...componentProps} />
-    </Suspense>
-  )
+const Renderer = ({ componentSrc }: { componentSrc: string }) => {
+  return <Iframe src={componentSrc} />
 }
 
 export default Renderer
