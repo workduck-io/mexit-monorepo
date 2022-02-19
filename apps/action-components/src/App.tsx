08a1e0d19ba86050d7592c82d72ff382d7549c2d
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-02-19 20:36:23 +0530

    generated action components boilerplate; made some changes to make it similar to cra;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

---
+++ apps/action-components/src/App.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+import { Route, Link, Routes } from 'react-router-dom'
+import ColourPicker from './components/ColourPicker'
+import CorporateBS from './components/CorpBS'
+
+const StyledApp = styled.div`
+  // Your style here
+`
+
+export function App() {
+  return (
+    <StyledApp>
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/color-picker" element={<ColourPicker />} />
+        <Route path="/corpbs" element={<CorporateBS />} />
+      </Routes>
+    </StyledApp>
+  )
+}
+
+export default App
