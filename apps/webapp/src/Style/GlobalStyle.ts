5fda4a624dcf5b524989326040362cc338bf9094
Mukul Mehta <hello@mukul-mehta.in>
2022-02-26 19:51:52 +0530

    Update global style from mex electron

--- apps/webapp/src/Style/GlobalStyle.ts
+++ apps/webapp/src/Style/GlobalStyle.ts
@@ -1,24 +1,84 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
+
+import normalize from './Normalize'
+import { ThinScrollbar } from './Helpers'
+import { EditorBalloonStyles, TippyBalloonStyles } from './Toolbar'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: ${({ theme }) => theme.colors.background.modal};
-    color: ${({ theme }) => theme.colors.text.default};
+  ${normalize}; // NormalizeCSS normalization
 
+  input:focus-visible {
+    outline: ${({ theme }) => theme.colors.primary} solid 1px;
+  }
+
+  *::placeholder {
+    color: ${({ theme }) => theme.colors.text.fade};
+    opacity: 0.5;
+  }
+
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
     height: 100vh;
-    margin: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    width: 100vw;
+    overflow: hidden;
+    display: flex;
+    font-family: Inter, sans-serif;
+    color: ${({ theme }) => theme.colors.text.heading};
+    ${({ theme }) => {
+      if (theme.backgroundImages) {
+        return css`
+          background-color: ${({ theme }) => theme.colors.background.app};
+          background-image: url(${theme.backgroundImages.app});
+          background-size: cover;
+        `
+      }
+
+      return css`
+        background: ${({ theme }) => theme.colors.background.app};
+      `
+    }}
+
+    * {
+      ${ThinScrollbar};
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.primary};
+
+    &:hover {
+      text-decoration: underline;
+      text-decoration-color: ${({ theme }) => theme.colors.primary};
+    }
   }
 
   #root {
-    height: 100vh;
+    width: 100%;
   }
 
-  html {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-    sans-serif;
+  /* Tippy Balloon styles */
+  ${TippyBalloonStyles}
+
+  /* Editor Balloon styles */
+  ${EditorBalloonStyles}
+
+  body > ul[role="listbox"]{
+    display: block;
+    /* list-style-type: disc; */
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 0px;
+  }
+
+  button {
+    border: none;
   }
 `
 
