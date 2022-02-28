91a2f8547fed55d1164e08f78958de6bb0b9c023
Mukul Mehta <hello@mukul-mehta.in>
2022-03-01 01:26:29 +0530

    Use vite for bundling webapp

---
+++ apps/webapp/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const path = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    sourcemap: true
+  },
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      '@mexit/shared': path.resolve(__dirname, '../../libs/shared/src')
+    }
+  },
+  plugins: [react()]
+})
