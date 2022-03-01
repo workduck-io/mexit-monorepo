bc524d10e3331ccdf278efa7fb6057e6ad16df2e
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-03-01 22:40:58 +0530

    Sahil wd 941 task mex editor in extension (#5)
    https://github.com/workduck-io/mexit-monorepo/pull/5
    * deps: added mex-editor package to extension;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * Added mex-editor;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * sputlit setting content only on save click;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>
    
    * added and then commented out plugins;
    
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Components/Editor.tsx
+++ apps/extension/src/Components/Editor.tsx
@@ -1,8 +1,11 @@
-import { Plate, usePlateEditorRef, createPlugins, createPlateUI } from '@udecode/plate'
+import { createPlugins, createPlateUI } from '@udecode/plate'
+import { MexEditor, ComboboxKey } from '@workduck-io/mex-editor'
+import { MexEditorOptions } from '@workduck-io/mex-editor/lib/types/editor'
+import { useDebouncedCallback } from 'use-debounce'
+
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { toast, Toaster } from 'react-hot-toast'
-import { nanoid } from 'nanoid'
 import { debounce } from 'lodash'
 
 import { useContentStore } from '../Hooks/useContentStore'
@@ -12,79 +15,37 @@ import generatePlugins from '../Utils/plugins'
 import { Button } from '@mexit/shared'
 import { closeSputlit } from '@mexit/shared'
 import { useAuthStore } from '../Hooks/useAuth'
-import { checkMetaParseableURL, parsePageMetaTags } from '@mexit/shared'
+import { checkMetaParseableURL, parsePageMetaTags, useTagStore } from '@mexit/shared'
 import { Tag } from '../Types/Tags'
 import config from '../config'
 
 const EditorWrapper = styled.div`
+  max-height: 100%;
+  overflow: scroll;
   margin: 1rem;
 `
 
-const Container = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`
+interface EditorProps {
+  content: any[] // eslint-disable-line @typescript-eslint/no-explicit-any
+  nodePath?: string
+  nodeUID: string
+  readOnly?: boolean
+  onChange?: any
+  autoFocus?: boolean
+  handleSave: (payload: any) => void
+}
 
-const Editor = ({ nodeId, content, onChange }: { nodeId: string; content: NodeEditorContent; onChange }) => {
+export const Editor: React.FC<EditorProps> = ({ nodeUID, nodePath, content, readOnly, onChange, handleSave }) => {
   const currTabURL = window.location.href
   const [pageMetaTags, setPageMetaTags] = useState<any[]>([])
-  // TODO: use Dinesh's editor package and take tags from editor
   const [userTags, setUserTags] = useState<Tag[]>([])
-  const mexit_content = useContentStore((state) => state.getContent(window.location.href))
-  const [nodePath, setNodePath] = useState<string>(config.mex.ACTIVITY_NODE_NAME)
 
-  const initialValue = [
-    {
-      children: content
-    }
-  ]
+  const addTags = useTagStore((store) => store.addTags)
+  const tags = useTagStore((store) => store.tags)
 
   const plugins = createPlugins(generatePlugins(), { components: createPlateUI() })
   const userDetails = useAuthStore((store) => store.userDetails)
   const workspaceDetails = useAuthStore((store) => store.workspaceDetails)
-
-  useEditorChange(nodeId, content)
-
-  const handleSave = () => {
-    const reqBody = {
-      id: userDetails.activityNodeUID ? userDetails.activityNodeUID : 'NODE_ACTIVITY',
-      nodePath: nodePath,
-      type: nodePath !== config.mex.ACTIVITY_NODE_NAME ? CaptureType.HIERARCHY : CaptureType.DRAFT,
-      createdBy: userDetails.userId,
-      workspace: workspaceDetails.name,
-      content: mexit_content,
-      metadata: {
-        metaTags: pageMetaTags,
-        userTags: userTags
-      }
-    }
-
-    chrome.runtime.sendMessage(
-      {
-        type: 'CAPTURE_HANDLER',
-        subType: 'CREATE_CONTENT_QC',
-        data: {
-          body: reqBody
-        }
-      },
-      (response) => {
-        const { message, error } = response
-        if (error) {
-          if (error === 'Not Authenticated') {
-            toast.error('Not Authenticated. Please login via Popup')
-          } else {
-            toast.error('An Error Occured. Please try again')
-          }
-        } else {
-          console.log('Successful')
-          toast.success('Successful!', { duration: 2000 })
-          setTimeout(() => {
-            closeSputlit()
-          }, 2000)
-        }
-      }
-    )
-  }
 
   useEffect(() => {
     if (checkMetaParseableURL(currTabURL)) {
@@ -93,24 +54,87 @@ const Editor = ({ nodeId, content, onChange }: { nodeId: string; content: NodeEd
     }
   }, [currTabURL])
 
-  const handlePathChange = (e: any) => {
-    e.preventDefault()
-    setNodePath(e.target.value)
+  useEditorChange(nodeUID, content)
+
+  const comboboxConfig = {
+    onKeyDownConfig: {
+      keys: {
+        tag: {
+          newItemHandler: (tag: string) => addTags({ id: 'TAG_1234', text: tag })
+        },
+        ilink: {
+          newItemHandler: (ilink: string, parentId?: string) => console.log(`ilink: ${ilink} | ParentID: ${parentId}`)
+        }
+      },
+      slashCommands: {}
+    },
+    onChangeConfig: {
+      tag: {
+        cbKey: ComboboxKey.TAG,
+        trigger: '#',
+        data: tags.map((t) => ({ ...t, text: t.text })),
+        icon: 'add-something-here'
+      },
+      ilink: {
+        cbKey: ComboboxKey.ILINK,
+        trigger: '[[',
+        data: [],
+        icon: 'add-something-here'
+      }
+    }
   }
 
-  return (
-    <>
-      <EditorWrapper>
-        <Plate id={nodeId} value={initialValue} plugins={plugins} />
+  const editorOptions: MexEditorOptions = {
+    editableProps: {
+      readOnly: false,
+      placeholder: "Let's try something here...",
+      autoFocus: true
+    },
+    focusOptions: {
+      edge: 'start',
+      focus: true
+    }
+  }
 
-        <input onChange={debounce((e) => handlePathChange(e), 1000)} placeholder="Hierarchy for Node" />
-        <Container>
-          <Button onClick={handleSave} type="submit" value="Save" />
-        </Container>
-      </EditorWrapper>
-      <Toaster position="bottom-center" />
-    </>
+  const debounced = useDebouncedCallback((value) => {
+    const f = !readOnly && typeof onChange === 'function' ? onChange : () => undefined
+    f(value)
+  }, 1000)
+
+  return (
+    <EditorWrapper>
+      <MexEditor
+        comboboxConfig={comboboxConfig}
+        meta={{
+          path: nodePath
+        }}
+        // TODO: adding plugins below breaks things
+        // plugins={plugins}
+        onChange={debounced}
+        options={editorOptions}
+        editorId={nodeUID}
+        value={content}
+      />
+
+      <button
+        onClick={() => {
+          const payload = {
+            id: userDetails.activityNodeUID ? userDetails.activityNodeUID : 'NODE_ACTIVITY',
+            nodePath: nodePath,
+            type: nodePath !== config.mex.ACTIVITY_NODE_NAME ? CaptureType.HIERARCHY : CaptureType.DRAFT,
+            createdBy: userDetails.userId,
+            workspace: workspaceDetails.name,
+            metadata: {
+              metaTags: pageMetaTags,
+              userTags: userTags
+            }
+          }
+
+          handleSave(payload)
+        }}
+      >
+        Save
+      </button>
+    </EditorWrapper>
   )
 }
-
-export default Editor
