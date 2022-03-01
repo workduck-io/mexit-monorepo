40ddc0673a2150c1b8cf569b65ba97cb6e81ada4
Mukul Mehta <hello@mukul-mehta.in>
2022-03-01 22:58:27 +0530

    Use code chunking in vite

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const path = require('path')
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
-    outDir: '../../dist/apps/webapp'
+    outDir: '../../dist/apps/webapp',
+    rollupOptions: {
+      input: [
+        path.resolve(__dirname, 'src', 'index.tsx'),
+        path.resolve(__dirname, 'src', 'Actions/Components', 'AliasWrapper.tsx')
+      ],
+      output: {
+        format: 'es',
+        dir: './'
+      }
+    }
   },
   define: {
     'process.env': {}
@@ -15,7 +24,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@mexit/shared': path.resolve(__dirname, '../../libs/shared/src')
-    }
+    },
+    dedupe: ['styled-components', 'react', 'react-dom', '@workduck-io/mex-editor', '@udecode/plate']
   },
   plugins: [react()]
 })
