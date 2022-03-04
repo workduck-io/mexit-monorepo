696a5941aec1807827aef2918c68159fae8dcf7f
Mukul Mehta <hello@mukul-mehta.in>
2022-03-05 00:40:45 +0530

    Add release extension github action (#7)
    https://github.com/workduck-io/mexit-monorepo/pull/7

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import styled, { ThemeProvider } from 'styled-components'
 
