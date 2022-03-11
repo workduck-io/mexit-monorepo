d57e6354221b9acc78ad4b04052e8706bae62ba6
Mukul Mehta <hello@mukul-mehta.in>
2022-03-11 16:39:10 +0530

    Fetch and render public node in readonly editor view

--- apps/link-sharing/src/Components/PublicNodeEditor.tsx
+++ apps/link-sharing/src/Components/PublicNodeEditor.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import styled from 'styled-components'
-// import { Plate } from '@udecode/plate'
+import { Plate, usePlateEditorRef } from '@udecode/plate'
 
 import Editor from './Editor'
 import { DEFAULT_CONTENT, WORKSPACE_NAME } from '../Data/defaults'
@@ -17,8 +17,10 @@ const SPlate = styled.div`
 
 const PublicNodeEditor = ({ nodeId }) => {
   const [nodeContent, setNodeContent] = useState<any[]>()
+  const [nodePath, setNodePath] = useState<string>(nodeId)
   const [isLoading, setIsLoading] = useState(false)
 
+  const editor = usePlateEditorRef()
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -26,12 +28,13 @@ const PublicNodeEditor = ({ nodeId }) => {
     async function getPublicNodeContent() {
       const URL = apiURLs.getPublicNode(nodeId)
       try {
-        const res = await axios.get(URL, {
+        const res = await axios.get<any>(URL, {
           headers: {
             'workspace-id': WORKSPACE_NAME
           }
         })
         setNodeContent(res.data.content)
+        setNodePath(res.data.path ?? nodeId)
       } catch (error) {
         navigate('/404')
       }
@@ -40,9 +43,14 @@ const PublicNodeEditor = ({ nodeId }) => {
     setIsLoading(false)
   }, [setNodeContent, setIsLoading]) // eslint-disable-line react-hooks/exhaustive-deps
 
+  useEffect(() => {
+    if (editor) editor.children = nodeContent
+
+    console.log('Node Content updated: ', nodeContent)
+  }, [editor, nodeContent])
+
   return (
     <SPlate>
-      {/* <Plate id={!isLoading ? nodeId : 'NODE_LOADING'} value={nodeContent ?? DEFAULT_CONTENT.content} /> */}
       <Editor
         nodeUID={!isLoading ? nodeId : 'NODE_LOADING'}
         nodePath={nodeId}
