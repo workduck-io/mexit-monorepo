7e6da42899d66e9e39014a9190a7a045c8a04ef1
Mukul Mehta <hello@mukul-mehta.in>
2022-03-14 20:41:32 +0530

    Add multiple search indexes mechanism from Mex

--- apps/webapp/src/Hooks/useSearchStore.ts
+++ apps/webapp/src/Hooks/useSearchStore.ts
@@ -1,128 +1,196 @@
 import create from 'zustand'
-import { persist } from 'zustand/middleware'
 
-import { BlockIndexData, Node, Block, FlexSearchResult } from '../Types/Data'
-import { Document } from 'flexsearch'
-import { createFlexsearchIndex, parseNode, parseBlock } from '../Utils/flexsearch'
+import { mog } from '@mexit/shared'
 
-interface BlockMapData {
-  text: string
-  nodeUID: string
-}
+import { createSearchIndex } from '../Utils/flexsearch'
+import { ILink } from '../Types/Data'
+import { SearchIndex, GenericSearchData, GenericSearchResult } from '../Types/Search'
+import { diskIndex, indexNames } from './../Data/constants'
+
+// interface BlockMapData {
+//   text: string
+//   nodeUID: string
+// }
+
+// interface SearchStoreState {
+//   blocks: Map<string, BlockMapData>
+//   nodeBlockRevIndex: Map<string, Set<string>>
+//   index: Document<BlockIndexData> | null
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   initializeSearchIndex: (initList: BlockIndexData[]) => Document<BlockIndexData>
+//   addBlock: (block: BlockIndexData) => void
+//   addMultipleBlocks: (blocks: BlockIndexData[]) => void
+//   addNode: (node: Node) => void
+//   removeBlock: (blockUID: string) => void
+//   removeNode: (nodeUID: string) => void
+//   updateBlock: (newBlock: Block) => void
+//   updateNode: (newNode: Node) => void
+//   fetchBlockByID: (blockUID: string) => BlockIndexData
+//   searchIndex: (query: string) => FlexSearchResult[]
+//   //   fetchIndexLocalStorage: () => void
+// }
+
+// const useSearchStore = create<SearchStoreState>(
+//   persist(
+//     (set, get) => ({
+//       blocks: new Map<string, BlockMapData>(),
+//       nodeBlockRevIndex: new Map<string, Set<string>>(),
+//       index: null,
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//       initializeSearchIndex: (initList: BlockIndexData[]) => {
+//         const index = createFlexsearchIndex(initList)
+//         set({ index })
+//         initList.forEach((block) => {
+//           get().blocks.set(block.blockUID, { text: block.text, nodeUID: block.nodeUID })
+//           if (block.nodeUID in get().nodeBlockRevIndex) {
+//             const updated = get().nodeBlockRevIndex.get(block.nodeUID).add(block.blockUID)
+//             get().nodeBlockRevIndex.set(block.nodeUID, updated)
+//           } else {
+//             const val = new Set<string>([block.blockUID])
+//             get().nodeBlockRevIndex.set(block.nodeUID, val)
+//           }
+//         })
+//         return index
+//       },
+//       addBlock: (block: BlockIndexData) => {
+//         get().blocks.set(block.blockUID, { text: block.text, nodeUID: block.nodeUID })
+//         get().index.add(block)
+
+//         if (block.nodeUID in get().nodeBlockRevIndex) {
+//           const updated = get().nodeBlockRevIndex.get(block.nodeUID).add(block.blockUID)
+//           get().nodeBlockRevIndex.set(block.nodeUID, updated)
+//         } else {
+//           const val = new Set<string>([block.blockUID])
+//           get().nodeBlockRevIndex.set(block.nodeUID, val)
+//         }
+//       },
+//       addMultipleBlocks: (blocks: BlockIndexData[]) => {
+//         blocks.forEach((block) => get().addBlock(block))
+//       },
+//       addNode: (node: Node) => {
+//         const blocks = parseNode(node)
+//         blocks.forEach((block) => get().addBlock(block))
+//       },
+//       removeBlock: (blockUID: string) => {
+//         const blockData = get().blocks.get(blockUID)
+//         get().index.remove(blockUID)
+//         get().blocks.delete(blockUID)
+
+//         const nodeUID = blockData.nodeUID
+//         const updated = get().nodeBlockRevIndex.get(nodeUID)
+//         updated.delete(blockUID)
+//         get().nodeBlockRevIndex.set(nodeUID, updated)
+//       },
+//       removeNode: (nodeUID: string) => {
+//         const blocks = get().nodeBlockRevIndex.get(nodeUID)
+//         blocks.forEach((block) => get().removeBlock(block))
+//       },
+//       updateBlock: (newBlock: Block) => {
+//         const blockText = parseBlock(newBlock.children)
+//         get().blocks.set(newBlock.id, { text: blockText, nodeUID: newBlock.nodeUID })
+//         get().index.update({
+//           blockUID: newBlock.id,
+//           nodeUID: newBlock.nodeUID,
+//           text: blockText
+//         })
+//       },
+//       updateNode: (newNode: Node) => {
+//         get().removeNode(newNode.id)
+//         get().addNode(newNode)
+//       },
+//       fetchBlockByID: (blockUID: string) => {
+//         const blockData = get().blocks.get(blockUID)
+//         return {
+//           blockUID: blockUID,
+//           nodeUID: blockData.nodeUID,
+//           text: blockData.text
+//         }
+//       },
+//       searchIndex: (query: string) => {
+//         const response = get().index.search(query)
+//         const results = new Array<any>()
+//         response.forEach((entry) => {
+//           const matchField = entry.field
+//           entry.result.forEach((i) => {
+//             const t: FlexSearchResult = {
+//               ...get().fetchBlockByID(i),
+//               matchField
+//             }
+//             results.push(t)
+//           })
+//         })
+
+//         return results
+//       }
+//     }),
+//     { name: 'mexit-search-index' }
+//   )
+// )
 
 interface SearchStoreState {
-  blocks: Map<string, BlockMapData>
-  nodeBlockRevIndex: Map<string, Set<string>>
-  index: Document | null
+  // docs: Map<string, NodeTitleText>
+  index: SearchIndex
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  initializeSearchIndex: (initList: BlockIndexData[]) => Document
-  addBlock: (block: BlockIndexData) => void
-  addMultipleBlocks: (blocks: BlockIndexData[]) => void
-  addNode: (node: Node) => void
-  removeBlock: (blockUID: string) => void
-  removeNode: (nodeUID: string) => void
-  updateBlock: (newBlock: Block) => void
-  updateNode: (newNode: Node) => void
-  fetchBlockByID: (blockUID: string) => BlockIndexData
-  searchIndex: (query: string) => FlexSearchResult[]
-  //   fetchIndexLocalStorage: () => void
+  initializeSearchIndex: (ilinks: ILink[], initData: Record<indexNames, any[]>) => SearchIndex
+  addDoc: (idxName: keyof SearchIndex, doc: GenericSearchData) => void
+  removeDoc: (idxName: keyof SearchIndex, id: string) => void
+  updateDoc: (idxName: keyof SearchIndex, newDoc: GenericSearchData) => void
+  searchIndex: (idxName: keyof SearchIndex, query: string) => GenericSearchResult[]
 }
-
-const useSearchStore = create<SearchStoreState>(
-  persist(
-    (set, get) => ({
-      blocks: new Map<string, BlockMapData>(),
-      nodeBlockRevIndex: new Map<string, Set<string>>(),
-      index: null,
+export const useSearchStore = create<SearchStoreState>((set, get) => ({
+  index: diskIndex,
+  initializeSearchIndex: (ilinks, initData) => {
+    const index = createSearchIndex(ilinks, initData)
+    set({ index })
+    return index
+  },
+  addDoc: (idxName, newDoc) => {
+    if (get().index[idxName]) {
+      const idx = get().index[idxName].add(newDoc)
+      set({ index: { ...get().index, [idxName]: idx } })
+    }
+  },
+  removeDoc: (idxName, id) => {
+    const idx = get().index[idxName].remove(id)
+    set({ index: { ...get().index, [idxName]: idx } })
+  },
+  updateDoc: (idxName, newDoc) => {
+    const idx = get().index[idxName].update(newDoc)
+    set({ index: { ...get().index, [idxName]: idx } })
+  },
+  searchIndex: (idxName, query) => {
+    try {
+      const response = get().index[idxName].search(query)
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      initializeSearchIndex: (initList: BlockIndexData[]) => {
-        const index = createFlexsearchIndex(initList)
-        set({ index })
-        initList.forEach((block) => {
-          get().blocks.set(block.blockUID, { text: block.text, nodeUID: block.nodeUID })
-          if (block.nodeUID in get().nodeBlockRevIndex) {
-            const updated = get().nodeBlockRevIndex.get(block.nodeUID).add(block.blockUID)
-            get().nodeBlockRevIndex.set(block.nodeUID, updated)
-          } else {
-            const val = new Set<string>([block.blockUID])
-            get().nodeBlockRevIndex.set(block.nodeUID, val)
-          }
+      const results = new Array<any>()
+      response.forEach((entry) => {
+        const matchField = entry.field
+        entry.result.forEach((i) => {
+          mog('ResultEntry', { i })
+          results.push({ id: i, matchField })
         })
-        return index
-      },
-      addBlock: (block: BlockIndexData) => {
-        get().blocks.set(block.blockUID, { text: block.text, nodeUID: block.nodeUID })
-        get().index.add(block)
+      })
 
-        if (block.nodeUID in get().nodeBlockRevIndex) {
-          const updated = get().nodeBlockRevIndex.get(block.nodeUID).add(block.blockUID)
-          get().nodeBlockRevIndex.set(block.nodeUID, updated)
+      const combinedResults = new Array<GenericSearchResult>()
+      results.forEach(function (item) {
+        const existing = combinedResults.filter(function (v, i) {
+          return v.id === item.id
+        })
+        if (existing.length) {
+          const existingIndex = combinedResults.indexOf(existing[0])
+          combinedResults[existingIndex].matchField = combinedResults[existingIndex].matchField.concat(item.matchField)
         } else {
-          const val = new Set<string>([block.blockUID])
-          get().nodeBlockRevIndex.set(block.nodeUID, val)
+          if (typeof item.matchField == 'string') item.matchField = [item.matchField]
+          combinedResults.push(item)
         }
-      },
-      addMultipleBlocks: (blocks: BlockIndexData[]) => {
-        blocks.forEach((block) => get().addBlock(block))
-      },
-      addNode: (node: Node) => {
-        const blocks = parseNode(node)
-        blocks.forEach((block) => get().addBlock(block))
-      },
-      removeBlock: (blockUID: string) => {
-        const blockData = get().blocks.get(blockUID)
-        get().index.remove(blockUID)
-        get().blocks.delete(blockUID)
+      })
 
-        const nodeUID = blockData.nodeUID
-        const updated = get().nodeBlockRevIndex.get(nodeUID)
-        updated.delete(blockUID)
-        get().nodeBlockRevIndex.set(nodeUID, updated)
-      },
-      removeNode: (nodeUID: string) => {
-        const blocks = get().nodeBlockRevIndex.get(nodeUID)
-        blocks.forEach((block) => get().removeBlock(block))
-      },
-      updateBlock: (newBlock: Block) => {
-        const blockText = parseBlock(newBlock.children)
-        get().blocks.set(newBlock.id, { text: blockText, nodeUID: newBlock.nodeUID })
-        get().index.update({
-          blockUID: newBlock.id,
-          nodeUID: newBlock.nodeUID,
-          text: blockText
-        })
-      },
-      updateNode: (newNode: Node) => {
-        get().removeNode(newNode.id)
-        get().addNode(newNode)
-      },
-      fetchBlockByID: (blockUID: string) => {
-        const blockData = get().blocks.get(blockUID)
-        return {
-          blockUID: blockUID,
-          nodeUID: blockData.nodeUID,
-          text: blockData.text
-        }
-      },
-      searchIndex: (query: string) => {
-        const response = get().index.search(query)
-        const results = new Array<any>()
-        response.forEach((entry) => {
-          const matchField = entry.field
-          entry.result.forEach((i) => {
-            const t: FlexSearchResult = {
-              ...get().fetchBlockByID(i),
-              matchField
-            }
-            results.push(t)
-          })
-        })
-
-        return results
-      }
-    }),
-    { name: 'mexit-search-index' }
-  )
-)
-
+      return combinedResults
+    } catch (e) {
+      mog('Searching Broke:', { e })
+      return []
+    }
+    return []
+  }
+}))
 export default useSearchStore
--- apps/webapp/src/Utils/flexsearch.ts
+++ apps/webapp/src/Utils/flexsearch.ts
@@ -1,5 +1,10 @@
+import { Snippet } from './../Stores/useSnippetStore'
 import { Document } from 'flexsearch'
 import { BlockIndexData, Node } from '../Types/Data'
+
+import { indexNames } from '../Data/constants'
+import { GenericSearchData, GenericSearchResult, SearchIndex } from '../Types/Search'
+import { ILink } from '../Types/Data'
 
 export const createFlexsearchIndex = (initList: BlockIndexData[]) => {
   const options = {
@@ -40,17 +45,88 @@ export const parseBlock = (content: any[], join?: string): string => {
   return text.join(join ?? '')
 }
 
-export const parseNode = (node: Node): BlockIndexData[] => {
+export const parseNode = (node: Node, title: string): GenericSearchData[] => {
   const nodeUID = node.id
-  const result: BlockIndexData[] = []
+  const result: GenericSearchData[] = []
 
   node.content.forEach((block) => {
     const blockText = parseBlock(block.children)
     if (blockText.length !== 0) {
-      const temp: BlockIndexData = { blockUID: block.id, text: blockText, nodeUID: nodeUID }
+      const temp: GenericSearchData = { id: block.id, text: blockText, nodeUID: nodeUID, title: title }
       result.push(temp)
     }
   })
 
   return result
+}
+
+export const parseSnippet = (snippet: Snippet): GenericSearchData => {
+  const snip: GenericSearchData = {
+    id: snippet.id,
+    title: snippet.title,
+    text: parseBlock(snippet.content)
+  }
+  return snip
+}
+
+export const createSearchIndex = (ilinks, initData: Record<indexNames, any[]>): SearchIndex => {
+  const initList: Record<indexNames, any> = convertDataToIndexable(ilinks, initData)
+  // Pass options corrwectly depending on what fields are indexed ([title, text] for now)
+  return {
+    node: createGenricSearchIndex(initList.node),
+    snippet: createGenricSearchIndex(initList.snippet),
+    archive: createGenricSearchIndex(initList.archive)
+  }
+}
+
+export const createGenricSearchIndex = (
+  initList: GenericSearchData[],
+  options: any = {
+    document: {
+      id: 'id',
+      index: ['title', 'text']
+    },
+    tokenize: 'full'
+  }
+): Document<GenericSearchData> => {
+  const index = new Document<GenericSearchData>(options)
+  initList.forEach((i) => index.add(i))
+  return index
+}
+
+export const convertDataToIndexable = (
+  ilinks: ILink[],
+  data: Record<indexNames, any[]>
+): Record<indexNames, GenericSearchData[]> => {
+  const titleNodeMap = new Map<string, string>()
+
+  ilinks.forEach((ilink) => {
+    titleNodeMap.set(ilink.nodeid, ilink.path)
+  })
+
+  const nodeData = data.node
+  const archiveData = data.archive
+  const snippetData = data.snippet
+
+  const res = {
+    node: [],
+    archive: [],
+    snippet: []
+  }
+
+  nodeData.forEach((node) => {
+    const blocks = parseNode(node, titleNodeMap.get(node.id))
+    res.node.concat(blocks)
+  })
+
+  archiveData.forEach((archivedNode) => {
+    const blocks = parseNode(archivedNode, titleNodeMap.get(archivedNode.id))
+    res.archive.concat(blocks)
+  })
+
+  snippetData.forEach((snippet) => {
+    res.snippet.push(parseSnippet(snippet))
+  })
+
+  return res
 }
