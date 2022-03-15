b0ca584a4b90da6f24632a05a4298817ba939ac3
Mukul Mehta <hello@mukul-mehta.in>
2022-03-15 20:46:36 +0530

    Set cookie on visiting share endpoint and check for that cookie when registering

--- apps/webapp/src/Views/PublicNodeView.tsx
+++ apps/webapp/src/Views/PublicNodeView.tsx
@@ -3,9 +3,16 @@ import { useParams } from 'react-router-dom'
 
 import PublicNodeEditor from '../Components/Editor/PublicNodeEditor'
 import PublicNavbar from '../Components/PublicNavbar'
+import Cookies from 'universal-cookie'
 
 const PublicNodeView = () => {
   const nodeId = useParams().nodeId
+
+  useEffect(() => {
+    const cookies = new Cookies()
+    const timestamp = Date.now()
+    cookies.set('mexit-sharing', timestamp, { path: '/' })
+  })
 
   return (
     <>
