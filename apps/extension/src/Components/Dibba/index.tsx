6d01ce424203ac6f40fe6b169b4119fb9df2a4de
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-03-17 15:24:27 +0530

    simple search for dibba;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Components/Dibba/index.tsx
+++ apps/extension/src/Components/Dibba/index.tsx
@@ -1,5 +1,5 @@
 import { LinkCapture, parseSnippet, Snippet } from '@mexit/shared'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSnippets } from '../../Hooks/useSnippets'
 import { useSputlitContext, VisualState } from '../../Hooks/useSputlitContext'
 import { ComboboxItem, ComboboxRoot, Img, ItemCenterWrapper, ItemDesc, ItemTitle } from './styled'
@@ -7,11 +7,16 @@ import { ComboboxItem, ComboboxRoot, Img, ItemCenterWrapper, ItemDesc, ItemTitle
 import quillPenLine from '@iconify-icons/ri/quill-pen-line'
 import { Icon } from '@iconify/react'
 import { useShortenerStore } from '../../Hooks/useShortener'
+import fuzzysort from 'fuzzysort'
 
 // TODO: whether or not to enable dibba should be a user's preference
 // we don't users to move away because they have doubts of us forcing a 'keylogger' on them
 export default function Dibba() {
   const { dibbaState, setDibbaState } = useSputlitContext()
+  const [query, setQuery] = useState('')
+  const [activeIndex, setActiveIndex] = useState<number>(0)
+  const [results, setResults] = useState([])
+
   const linkCaptures = useShortenerStore((store) => store.linkCaptures)
   const snippets = useSnippets().getSnippets()
 
@@ -26,12 +31,9 @@ export default function Dibba() {
     ...snippets
   ]
 
-  console.log('snippets', snippets, 'link Captures', linkCaptures)
-
   const insertSnippet = (item: Snippet) => {
     dibbaState.extra.range.deleteContents()
     dibbaState.extra.range.insertNode(document.createTextNode(parseSnippet(item).text))
-    setDibbaState({ visualState: VisualState.hidden })
   }
 
   const insertLink = (item: any) => {
@@ -41,7 +43,6 @@ export default function Dibba() {
 
     dibbaState.extra.range.deleteContents()
     dibbaState.extra.range.insertNode(link)
-    setDibbaState({ visualState: VisualState.hidden })
   }
 
   const handleClick = (item: any) => {
@@ -51,7 +52,58 @@ export default function Dibba() {
       // TODO: transform again to type linkCapture
       insertLink(item)
     }
+    setDibbaState({ visualState: VisualState.hidden })
   }
+
+  useEffect(() => {
+    if (query !== '') {
+      const res = fuzzysort.go(query, data, { key: 'title' }).map((item) => item.obj)
+      setResults(res)
+    } else {
+      setResults(data)
+    }
+
+    console.log('current query', query)
+  }, [query])
+
+  useEffect(() => {
+    setActiveIndex(0)
+  }, [results])
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === 'ArrowDown') {
+        event.preventDefault()
+
+        setActiveIndex(activeIndex < results.length - 1 ? activeIndex + 1 : activeIndex)
+      } else if (event.key === 'ArrowUp') {
+        event.preventDefault()
+
+        setActiveIndex(activeIndex > 0 ? activeIndex - 1 : activeIndex)
+      } else if (event.key === 'Escape') {
+        event.preventDefault()
+
+        setDibbaState({ visualState: VisualState.hidden })
+      } else if (['Tab', 'Enter', ' ', ']'].includes(event.key)) {
+        event.preventDefault()
+
+        handleClick(results[activeIndex])
+      } else {
+        // TODO: fix or add something to know when the cursor has moved past the trigger
+        if (event.key === 'Backspace') {
+          setQuery(query.slice(0, -1))
+        } else {
+          setQuery(query + event.key)
+        }
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [activeIndex, results])
+
+  useEffect(() => console.log('active index', activeIndex), [activeIndex])
 
   return (
     <ComboboxRoot
@@ -59,13 +111,12 @@ export default function Dibba() {
       left={window.scrollX + dibbaState.coordinates.left}
       isOpen={dibbaState.visualState === VisualState.hidden ? false : true}
     >
-      {data.map((item, index) => {
+      {results.map((item, index) => {
         return (
           <ComboboxItem
             key={index}
-            highlighted={false}
+            highlighted={index === activeIndex}
             onMouseDown={() => {
-              console.log('something', item)
               handleClick(item)
             }}
           >
