1730b15b540be919c61e23e77d5abcc08dbf9e4e
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-04-05 23:09:40 +0530

    added document lock to prevent background scroll;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- libs/shared/src/Utils/helpers.ts
+++ libs/shared/src/Utils/helpers.ts
@@ -21,3 +21,16 @@ export async function copyToClipboard(text: any) {
       toast.error('An error occurred. Please try again later')
     })
 }
+
+// https://stackoverflow.com/questions/13382516/getting-scroll-bar-width-using-javascript
+export function getScrollbarWidth() {
+  const outer = document.createElement('div')
+  outer.style.visibility = 'hidden'
+  outer.style.overflow = 'scroll'
+  document.body.appendChild(outer)
+  const inner = document.createElement('div')
+  outer.appendChild(inner)
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth
+  outer.parentNode!.removeChild(outer)
+  return scrollbarWidth
+}
