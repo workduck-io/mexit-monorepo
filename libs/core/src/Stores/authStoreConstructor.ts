448cc7fcf5026eb90164a2e63f5686dd29b9ec95
Rishivikram N <rishi.vikram.1@gmail.com>
2022-04-21 11:01:53 +0530

    Added Forgot Password OAuth Flow

--- libs/core/src/Stores/authStoreConstructor.ts
+++ libs/core/src/Stores/authStoreConstructor.ts
@@ -1,4 +1,5 @@
 export const authStoreConstructor = (set, get) => ({
+  isForgottenPassword: false,
   authenticated: false,
   registered: false,
   userDetails: undefined,
@@ -7,6 +8,7 @@ export const authStoreConstructor = (set, get) => ({
     set({ authenticated: true, userDetails, workspaceDetails, registered: false }),
   setUnAuthenticated: () => set({ authenticated: false, userDetails: undefined, workspaceDetails: undefined }),
   setRegistered: (val) => set({ registered: val }),
+  setIsForgottenPassword: (val) => set({ isForgottenPassword: val }),
   getWorkspaceId: () => {
     const workspaceDetails = get().workspaceDetails
     if (workspaceDetails) {
