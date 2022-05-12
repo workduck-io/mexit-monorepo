3147f7500aaff0ba6aaf105f0c8a004b64155998
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-05-12 16:48:37 +0530

    syncing other store states across tabs;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Stores/useThemeStore.ts
+++ apps/webapp/src/Stores/useThemeStore.ts
@@ -2,14 +2,7 @@ import { themeStoreConstructor } from '@mexit/shared'
 import { ThemeStoreState } from '@mexit/core'
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
-import { share, isSupported } from 'shared-zustand'
 
 const useThemeStore = create<ThemeStoreState>(persist(themeStoreConstructor, { name: 'mexit-theme-store' }))
-
-// progressive enhancement check.
-if ('BroadcastChannel' in globalThis /* || isSupported() */) {
-  // share the property "count" of the state with other tabs
-  share('theme', useThemeStore)
-}
 
 export default useThemeStore
