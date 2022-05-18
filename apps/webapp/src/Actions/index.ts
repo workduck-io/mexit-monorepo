22adf8510b9229dbcb37e2b618e1d59efc938a17
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-05-18 20:46:23 +0530

    removed currency convertor action;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Actions/index.ts
+++ apps/webapp/src/Actions/index.ts
@@ -1,5 +1,4 @@
 export { ColourPicker } from './Components/ColourPicker'
 export { CorporateBS } from './Components/CorpBS'
-export { CurrencyConverter } from './Components/CurrencyConverter'
 export { UnixEpochConverter } from './Components/UnixEpoch'
 export { Shortener } from './Components/Shortener'
