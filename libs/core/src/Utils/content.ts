bf865c0eeffb510665fc66d80a40520a80deae20
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-06-07 01:03:01 +0530

    fixed for no metadata;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- libs/core/src/Utils/content.ts
+++ libs/core/src/Utils/content.ts
@@ -77,8 +77,8 @@ export const extractMetadata = (data: any): NodeMetadata => {
     updatedAt: data.updatedAt,
     createdBy: data.createdBy,
     createdAt: data.createdAt,
-    saveableRange: data?.elementMetadata.saveableRange,
-    sourceUrl: data?.elementMetadata.sourceUrl
+    saveableRange: data?.elementMetadata?.saveableRange,
+    sourceUrl: data?.elementMetadata?.sourceUrl
   }
   return removeNulls(metadata)
 }
