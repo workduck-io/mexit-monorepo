0953e42a6664275fe5d36d7da9f9fae601c73c16
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-06-08 17:46:34 +0530

    removed unnecessary stuff from extension's mex-editor;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Editor/utils/index.ts
+++ apps/extension/src/Editor/utils/index.ts
@@ -1,47 +1,6 @@
-export { SEPARATOR } from './constants';
-export { getHugeDocument } from './content';
-export { getTextFromTrigger } from './getTextFromTrigger';
-export {
-  Settify,
-  getAllParentIds,
-  removeLink,
-  removeNulls,
-  typeInvert,
-  withoutDelimiter,
-} from './helper';
-export {
-  ID_SEPARATOR,
-  NODE_ID_PREFIX,
-  WORKSPACE_ID_PREFIX,
-  IG_ID_PREFIX,
-  SYNC_BLOCK_ID_PREFIX,
-  TEMP_ID_PREFIX,
-  SNIPPET_PREFIX,
-  SYNCTEMP_PREFIX,
-  TODO_PREFIX,
-  DRAFT_PREFIX,
-  MEETING_PREFIX,
-  QUESTION_ID_PREFIX,
-  DRAFT_NODE,
-  generateNodeUID,
-  generateNodeId,
-  generateWorkspaceId,
-  generateIgId,
-  generateSyncBlockId,
-  generateTempId,
-  generateSnippetId,
-  generateSyncTempId,
-  generateTodoId,
-  generateQuestionId,
-} from './idGenerators';
-export { fuzzySearch } from './lib';
-export { mog } from './mog';
-export { setElementPositionByRange } from './setElementPositionByRange';
-export {
-  action,
-  combineAndImmer,
-  createStore,
-  immer,
-  immerMutable,
-  setStoreValue,
-} from './store.utils';
+export { getHugeDocument } from './content'
+export { getTextFromTrigger } from './getTextFromTrigger'
+export { Settify, getAllParentIds, removeLink, removeNulls, typeInvert, withoutDelimiter } from './helper'
+export { fuzzySearch } from './lib'
+export { setElementPositionByRange } from './setElementPositionByRange'
+export { action, combineAndImmer, createStore, immer, immerMutable, setStoreValue } from './store.utils'
