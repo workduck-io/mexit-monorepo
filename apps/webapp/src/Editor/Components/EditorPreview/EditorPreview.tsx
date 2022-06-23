ddf1ac4e1fc8503a2af326b916f96f94a9333e5f
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-06-24 01:37:37 +0530

    cross icon always in editor previews;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Editor/Components/EditorPreview/EditorPreview.tsx
+++ apps/webapp/src/Editor/Components/EditorPreview/EditorPreview.tsx
@@ -113,11 +113,9 @@ const EditorPreview = ({
                   </EditorPreviewNoteName>
                 )}
                 <TagsRelatedTiny nodeid={nodeid} />
-                {allowClosePreview && (
-                  <Button transparent onClick={() => closePreview && closePreview()}>
-                    <Icon icon={closeCircleLine} />
-                  </Button>
-                )}
+                <Button transparent onClick={() => closePreview && closePreview()}>
+                  <Icon icon={closeCircleLine} />
+                </Button>
               </EditorPreviewControls>
             )}
             <EditorPreviewEditorWrapper>
