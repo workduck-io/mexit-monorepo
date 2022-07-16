de272fea135d147cabc7ec3fe2a5d54971d42eb0
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-07-16 05:36:06 +0530

    fixed access level getter; added conditionally rendering of edit and delete icon in tooltip;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Components/Tooltip/index.tsx
+++ apps/extension/src/Components/Tooltip/index.tsx
@@ -58,6 +58,7 @@ function Tooltip() {
   const mentionable = useMentionStore((state) => state.mentionable)
 
   const nodeId = highlighted[window.location.href][tooltipState.id].nodeId
+  const [access, setAccess] = useState<AccessLevel>()
 
   const { getUserFromUserid, getUserAccessLevelForNode } = useMentions()
   // const { getUserDetailsUserId } = useUserService()
@@ -66,12 +67,11 @@ function Tooltip() {
     if (isSharedNode(nodeId)) {
       const sharedNode = getSharedNode(nodeId)
       const u = getUserFromUserid(sharedNode?.owner)
+      setAccess(sharedNode.currentUserAccess)
 
       return u
     }
   }, [mentionable, cache, nodeId])
-
-  const access = getUserAccessLevelForNode(user?.email, nodeId)
 
   const handleDelete = () => {
     const content = getContent(nodeId)
@@ -144,40 +144,44 @@ function Tooltip() {
       left={window.scrollX + tooltipState.coordinates.left}
       showTooltip={tooltipState.visualState === VisualState.hidden ? false : true}
     >
-      <Icon onClick={handleEdit}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-        </svg>
-      </Icon>
+      {access !== 'READ' && (
+        <Icon onClick={handleEdit}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+          </svg>
+        </Icon>
+      )}
 
-      <Icon onClick={handleDelete}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <polyline points="3 6 5 6 21 6"></polyline>
-          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-          <line x1="10" y1="11" x2="10" y2="17"></line>
-          <line x1="14" y1="11" x2="14" y2="17"></line>
-        </svg>
-      </Icon>
+      {access !== 'READ' && (
+        <Icon onClick={handleDelete}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <polyline points="3 6 5 6 21 6"></polyline>
+            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+            <line x1="10" y1="11" x2="10" y2="17"></line>
+            <line x1="14" y1="11" x2="14" y2="17"></line>
+          </svg>
+        </Icon>
+      )}
 
       <Icon
         onClick={() =>
