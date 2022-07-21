7d25c1aa8ee49f83b9a9492f8081882d51f48ff5
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-07-21 16:07:20 +0530

    renamed data keys and removed logs; removed unnecessary while loop;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Utils/getProfileData.ts
+++ apps/extension/src/Utils/getProfileData.ts
@@ -1,53 +1,53 @@
 const data = {
   LinkedIN: {
-    name: '//h1[@class="text-heading-xlarge inline t-24 v-align-middle break-words"]',
-    headline: '//div[@class="text-body-medium break-words"]',
-    location: '//span[@class="text-body-small inline t-black--light break-words"]',
-    currentCompany: '//div[@aria-label="Current company"]',
-    education: '//div[@aria-label="Education"]',
-    about:
+    Name: '//h1[@class="text-heading-xlarge inline t-24 v-align-middle break-words"]',
+    Headline: '//div[@class="text-body-medium break-words"]',
+    Location: '//span[@class="text-body-small inline t-black--light break-words"]',
+    'Current Company': '//div[@aria-label="Current company"]',
+    Education: '//div[@aria-label="Education"]',
+    About:
       '//div[@class="display-flex ph5 pv3"]/div/div[@class="pv-shared-text-with-see-more t-14 t-normal t-black display-flex align-items-center"]/div/span[@aria-hidden="true"]',
-    noOfConections: '//li[@class="text-body-small"]/span',
-    mutualConnections: '//span[@class="t-normal t-black--light t-14 hoverable-link-text"]'
+    Connections: '//li[@class="text-body-small"]/span',
+    'Mutual Connections': '//span[@class="t-normal t-black--light t-14 hoverable-link-text"]'
     // exprience : '//div[@class="display-flex flex-row justify-space-between"]/div/div/span'
   },
   Instagram: {
-    name: '//h2[@class="_aacl _aacs _aact _aacx _aada"]',
-    stats: '//ul[@class="_aa_7"]',
-    bioData: '//div[@class="_aa_c"]'
+    Name: '//h2[@class="_aacl _aacs _aact _aacx _aada"]',
+    Stats: '//ul[@class="_aa_7"]',
+    Bio: '//div[@class="_aa_c"]'
   },
   GitHub: {
-    userName: '//span[@class="p-nickname vcard-username d-block"]',
-    userProfileBio: '//div[@class="p-note user-profile-bio mb-3 js-user-profile-bio f4"]/div',
-    userStats: '//div[@class="mb-3"]',
-    location: '//li[@class="vcard-detail pt-1 css-truncate css-truncate-target hide-sm hide-md"]',
-    profileWebsite: '//li[@class="vcard-detail pt-1 css-truncate css-truncate-target "]',
-    twitterId: '//li[@itemprop="twitter"]',
-    noRepositories: '//a[@class="UnderlineNav-item js-responsive-underlinenav-item"]/span'
+    Username: '//span[@class="p-nickname vcard-username d-block"]',
+    Bio: '//div[@class="p-note user-profile-bio mb-3 js-user-profile-bio f4"]/div',
+    Stats: '//div[@class="mb-3"]',
+    Location: '//li[@class="vcard-detail pt-1 css-truncate css-truncate-target hide-sm hide-md"]',
+    Website: '//li[@class="vcard-detail pt-1 css-truncate css-truncate-target "]',
+    Twitter: '//li[@itemprop="twitter"]',
+    'Number of Repositories': '//a[@class="UnderlineNav-item js-responsive-underlinenav-item"]/span'
   },
   Twitter: {
-    name: '//div[@class="css-901oao r-1awozwy r-18jsvk2 r-6koalj r-37j5jr r-adyw6z r-1vr29t4 r-135wba7 r-bcqeeo r-1udh08x r-qvutc0"]/span[@class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"]/span[@class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"]',
-    userId:
+    Name: '//div[@class="css-901oao r-1awozwy r-18jsvk2 r-6koalj r-37j5jr r-adyw6z r-1vr29t4 r-135wba7 r-bcqeeo r-1udh08x r-qvutc0"]/span[@class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"]/span[@class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"]',
+    Handle:
       '//div[@class="css-901oao css-bfa6kz r-18u37iz r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"]/span[@class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0"]',
-    userDiscription: '//div[@class="css-1dbjc4n"]/div[@data-testid="UserDescription"]',
-    userJoinDate: '//span[@data-testid="UserJoinDate"]/span',
-    following: '//div[@class="css-1dbjc4n r-1mf7evn"]/a/span',
-    followers:
+    Description: '//div[@class="css-1dbjc4n"]/div[@data-testid="UserDescription"]',
+    'Joining Date': '//span[@data-testid="UserJoinDate"]/span',
+    Following: '//div[@class="css-1dbjc4n r-1mf7evn"]/a/span',
+    Followers:
       '//div[@class="css-1dbjc4n"]/a[@class="css-4rbku5 css-18t94o4 css-901oao r-18jsvk2 r-1loqt21 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"]/span'
   },
   YouTube: {
-    chanelName: '//yt-formatted-string[@class="style-scope ytd-channel-name"]',
-    subscriberCount: '//yt-formatted-string[@id="subscriber-count"]',
-    discription: '//yt-formatted-string[@id="description"]',
-    joiningDate:
+    'Channel Name': '//yt-formatted-string[@class="style-scope ytd-channel-name"]',
+    'Subscriber Count': '//yt-formatted-string[@id="subscriber-count"]',
+    Description: '//yt-formatted-string[@id="description"]',
+    'Joining Date':
       '//div[@id="right-column"]/yt-formatted-string[@class="style-scope ytd-channel-about-metadata-renderer"]',
-    totalView: '//div[@id="right-column"]/yt-formatted-string[@no-styles]'
+    'Total Views': '//div[@id="right-column"]/yt-formatted-string[@no-styles]'
   },
   Slack: {
-    profileName: '//span[@class="p-r_member_profile__name__text"]',
-    profileSubtitle: '//div[@class="p-r_member_profile__subtitle"]',
-    emailAddress: '(//div[@class="p-rimeto_member_profile_field__value"])[1]',
-    phoneNumber: '(//div[@class="p-rimeto_member_profile_field__value"])[2]'
+    Name: '//span[@class="p-r_member_profile__name__text"]',
+    Bio: '//div[@class="p-r_member_profile__subtitle"]',
+    Email: '(//div[@class="p-rimeto_member_profile_field__value"])[1]',
+    Phone: '(//div[@class="p-rimeto_member_profile_field__value"])[2]'
   },
   Producthunt: {
     productName:
@@ -63,22 +63,22 @@ const data = {
       '//div[@class="style_flex___KlcI style_direction-column__w77hU style_flex-column-gap-4__87DvT style_pb-12__SqQ5M"]/div[@class="style_color-dark-grey__aN5DV style_fontSize-16__DCrgA style_fontWeight-400__5p97M"]'
   },
   Capterra: {
-    productName : '//h1[@class="sm:nb-type-2xl nb-type-xl"]',
-    companyName : '//h1[@class="sm:nb-type-2xl nb-type-xl"]',
+    productName: '//h1[@class="sm:nb-type-2xl nb-type-xl"]',
+    companyName: '//h1[@class="sm:nb-type-2xl nb-type-xl"]',
     rating: '//div[@class="nb-mr-xl"]/a/div/div[@class="nb-ml-3xs"]',
     about: '//div[@class="nb-px-xl md:nb-pr-xl md:nb-pl-0 md:nb-w-1/2"]',
     features: '//div[@class="nb-mb-xl nb-flex nb-flex-wrap"]',
     pricing: '//div[@class="nb-mb-2xs"]',
-    pricingDetails: '//div[@class="nb-text-gray-400 nb-leading-md nb-tracking-md nb-text-md nb-mb-xl"]',
+    pricingDetails: '//div[@class="nb-text-gray-400 nb-leading-md nb-tracking-md nb-text-md nb-mb-xl"]'
   },
-  G2:{
+  G2: {
     productName: '//a[@class="c-midnight-100"]',
     ratings: '//ul[@class="list--piped mb-0"]',
-    discription: '//div[@class="ws-pw"]',
-    "" : '(//div[@class="ml-1"])[3]',
-    pricing: '//div[@class="preview-cards preview-cards--responsive"]',
+    description: '//div[@class="ws-pw"]',
+    pricing: '//div[@class="preview-cards preview-cards--responsive"]'
   }
 }
+
 const ValidUrl = {
   1: {
     WebPage: 'LinkedIN',
@@ -112,7 +112,7 @@ const ValidUrl = {
     WebPage: 'Capterra',
     regex: /^(http(s)?:\/\/)?([\w]+\.)capterra\.com\/p\/[1-9]*/
   },
-  9:{
+  9: {
     WebPage: 'G2',
     regex: /^(http(s)?:\/\/)?([\w]+\.)g2\.com\/products/
   }
@@ -125,18 +125,17 @@ const checkURL = (url) => {
   }
   return null
 }
-const sendData = []
-const getUserData = async () => {
+
+export const getProfileData = async () => {
+  const sendData = []
   const url = window.location.href
-  console.log(url)
+
   let webPage = await checkURL(url)
-  console.log('webPage :', webPage)
-  while (sendData.length) sendData.pop()
   if (webPage !== null) {
     sendData.push({
       children: [
         {
-          text: 'profile data of webpage : ' + webPage
+          text: 'Profile data of webpage: ' + document.title
         },
         {
           text: ' ['
@@ -155,15 +154,15 @@ const getUserData = async () => {
         }
       ]
     })
+
     for (var d in data[webPage]) {
       const ele = document.evaluate(data[webPage][d], document, null, XPathResult.ANY_TYPE, null).iterateNext()
-      console.log(d)
+      // console.log(d)
       if (ele !== null) {
-        console.log(ele.textContent.trim())
         sendData.push({
           children: [
             {
-              text: d + ' : ' + ele.textContent.trim()
+              text: d + ': ' + ele.textContent.trim()
             },
             {
               text: '\n'
@@ -178,7 +177,3 @@ const getUserData = async () => {
   }
   return sendData
 }
-const getLinks = async () => {
-  return await getUserData()
-}
-export default getLinks
