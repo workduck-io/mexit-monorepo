ea2bb7d7b22e8c2b4be8a95d96d0f431372aa22f
Jay Budhadev <jbudhadev@gmail.com>
2022-08-27 19:48:41 +0530

    Icon Button:Extension

--- apps/extension/src/Editor/components/Toolbar/Toolbar.tsx
+++ apps/extension/src/Editor/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,6 @@
-import { Icon } from '@iconify/react';
+import React from 'react'
+
+import { Icon } from '@iconify/react'
 import {
   usePlateEditorRef,
   getPluginType,
@@ -14,68 +16,39 @@ import {
   ELEMENT_H3,
   ELEMENT_OL,
   ELEMENT_UL,
-  ListToolbarButton,
-} from '@udecode/plate';
-import React from 'react';
-import { Separator } from './Seperator';
+  ListToolbarButton
+} from '@udecode/plate'
 
-const IconButton = ({ icon }: { icon: string }) => (
-  <Icon icon={icon} fontSize="24" />
-);
+import { Separator } from './Seperator'
+
+const IconButton = ({ icon }: { icon: string }) => <Icon icon={icon} height={24} />
 
 const Toolbar = () => {
-  const editor = usePlateEditorRef();
-  const theme = 'dark';
-  const arrow = false;
+  const editor = usePlateEditorRef()
+  const theme = 'dark'
+  const arrow = false
 
   return (
     <BalloonToolbar theme={theme} arrow={arrow}>
-      <MarkToolbarButton
-        type={getPluginType(editor, MARK_BOLD)}
-        icon={<IconButton icon="ri:bold" />}
-      />
-      <MarkToolbarButton
-        type={getPluginType(editor, MARK_ITALIC)}
-        icon={<IconButton icon="ri:italic" />}
-      />
-      <MarkToolbarButton
-        type={getPluginType(editor, MARK_UNDERLINE)}
-        icon={<IconButton icon="ri:underline" />}
-      />
+      <MarkToolbarButton type={getPluginType(editor, MARK_BOLD)} icon={<Icon icon="ri:bold" height={24} />} />
+      <MarkToolbarButton type={getPluginType(editor, MARK_ITALIC)} icon={<Icon icon="ri:italic" height={24} />} />
+      <MarkToolbarButton type={getPluginType(editor, MARK_UNDERLINE)} icon={<Icon icon="ri:underline" height={24} />} />
 
       <Separator />
 
-      <BlockToolbarButton
-        type={getPluginType(editor, ELEMENT_H1)}
-        icon={<IconButton icon="ri:h-1" />}
-      />
+      <BlockToolbarButton type={getPluginType(editor, ELEMENT_H1)} icon={<Icon icon="ri:h-1" />} height={24} />
 
-      <BlockToolbarButton
-        type={getPluginType(editor, ELEMENT_H2)}
-        icon={<IconButton icon="ri:h-2" />}
-      />
+      <BlockToolbarButton type={getPluginType(editor, ELEMENT_H2)} icon={<Icon icon="ri:h-2" />} height={24} />
 
-      <BlockToolbarButton
-        type={getPluginType(editor, ELEMENT_H3)}
-        icon={<IconButton icon="ri:h-3" />}
-      />
+      <BlockToolbarButton type={getPluginType(editor, ELEMENT_H3)} icon={<Icon icon="ri:h-3" />} height={24} />
 
       <Separator />
 
-      <AlignToolbarButton
-        value="left"
-        icon={<IconButton icon="bx:bx-align-left" />}
-      />
-      <AlignToolbarButton
-        value="center"
-        icon={<IconButton icon="bx:bx-align-middle" />}
-      />
-      <AlignToolbarButton
-        value="right"
-        icon={<IconButton icon="bx:bx-align-right" />}
-      />
+      <AlignToolbarButton value="left" icon={<Icon icon="bx:bx-align-left" />} height={24} />
+      <AlignToolbarButton value="center" icon={<Icon icon="bx:bx-align-middle" />} height={24} />
+      <AlignToolbarButton value="right" icon={<Icon icon="bx:bx-align-right" />} height={24} />
     </BalloonToolbar>
-  );
-};
+  )
+}
 
-export default Toolbar;
+export default Toolbar
