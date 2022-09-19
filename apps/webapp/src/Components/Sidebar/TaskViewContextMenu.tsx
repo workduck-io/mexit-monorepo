f08d3dcf6fa8d558654ac71da35236daa35b0429
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-09-19 15:53:33 +0530

    Fixed task view context menu; Added wrapper over it's sidebar;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Components/Sidebar/TaskViewContextMenu.tsx
+++ apps/webapp/src/Components/Sidebar/TaskViewContextMenu.tsx
@@ -3,25 +3,28 @@ import React from 'react'
 import trashIcon from '@iconify/icons-codicon/trash'
 import fileCopyLine from '@iconify/icons-ri/file-copy-line'
 import { Icon } from '@iconify/react'
-import { ContextMenuContent, ContextMenuItem, ContextMenuSeparator } from '@radix-ui/react-context-menu'
 
 import { NavigationType, ROUTE_PATHS, useRouting } from '../../Hooks/useRouting'
-import { View, useViewStore } from '../../Hooks/useTaskViews'
+import { View, useViewStore, useTaskViews } from '../../Hooks/useTaskViews'
+import { ContextMenuContent, ContextMenuItem, ContextMenuSeparator } from '../../Style/contextMenu'
 import { useTaskViewModalStore } from '../TaskViewModal'
+import { SidebarListItem } from './SidebarList'
 
 interface TaskViewContextMenuProps {
-  item: View<any>
+  item: SidebarListItem<View<any>>
 }
 
 const TaskViewContextMenu = ({ item }: TaskViewContextMenuProps) => {
-  const removeView = useViewStore((store) => store.removeView)
+  // const removeView = useViewStore((store) => store.removeView)
   const openModal = useTaskViewModalStore((store) => store.openModal)
   const setCurrentView = useViewStore((store) => store.setCurrentView)
   const { goTo } = useRouting()
+  // const { deleteView } = useTaskViews()
 
   const handleDelete = async (view: View<any>) => {
     const currentView = useViewStore.getState().currentView
-    removeView(view.id)
+    // TODO: this is related to syncing tasks views with backend, needs to be done but now
+    // await deleteView(view.id)
     if (currentView?.id === view.id) {
       setCurrentView(undefined)
       goTo(ROUTE_PATHS.tasks, NavigationType.push)
@@ -37,7 +40,7 @@ const TaskViewContextMenu = ({ item }: TaskViewContextMenuProps) => {
       <ContextMenuContent>
         <ContextMenuItem
           onSelect={(args) => {
-            handleClone(item)
+            handleClone(item.data)
           }}
         >
           <Icon icon={fileCopyLine} />
@@ -47,7 +50,7 @@ const TaskViewContextMenu = ({ item }: TaskViewContextMenuProps) => {
         <ContextMenuItem
           color="#df7777"
           onSelect={(args) => {
-            handleDelete(item)
+            handleDelete(item.data)
           }}
         >
           <Icon icon={trashIcon} />
