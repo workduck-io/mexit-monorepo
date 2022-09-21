27c7b2bc6294490850561de7da868a3efbfc935d
Mukul Mehta <mukul@workduck.io>
2022-09-21 19:41:58 +0530

    Add public node view even in maintenance

--- apps/webapp/src/Switch.tsx
+++ apps/webapp/src/Switch.tsx
@@ -8,6 +8,7 @@ import RouteNotFound from './Components/404'
 import DownForMaintenance from './Components/DownForMaintenance'
 import GenericOAuthRedirect from './Components/OAuth/GenericOAuthRedirect'
 import { ROUTE_PATHS } from './Hooks/useRouting'
+import PublicNodeView from './Views/PublicNodeView'
 
 export const SwitchWrapper = styled(animated.div)`
   height: 100%;
@@ -28,6 +29,7 @@ export const Switch = () => {
     <SwitchWrapper>
       <Routes>
         <Route path={`${ROUTE_PATHS.oauth}/:serviceName`} element={<OAuthRoute />} />
+        <Route path={`${ROUTE_PATHS.share}/:nodeId`} element={<PublicNodeView />} />
         <Route path={`/*`} element={<DownForMaintenance />}></Route>
         <Route path="*" element={<RouteNotFound />} />
       </Routes>
