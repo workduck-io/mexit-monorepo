f29b58e9521824226a1df672daeda33e004e7b46
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-09-21 17:05:10 +0530

    basic styling;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,60 @@ import React from 'react'
 import styled from 'styled-components'
 import GlobalStyle from './Styles/GlobalStyle'
 
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+  padding: 1rem;
+
+  @media (max-width: 600px) {
+    justify-content: space-around;
+  }
+`
+
+const StyledFooter = styled.div`
+  font-weight: 400;
+`
+
 const Container = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 600px) {
+    flex-direction: column-reverse;
+
+    img {
+      width: 100%;
+    }
+  }
 `
 
 const App = () => {
   return (
-    <React.Fragment>
+    <StyledApp>
       <GlobalStyle />
+
       <Container>
-        <h1>Shhh....Going for a short nap to duck better. We can keep this a secret, right?</h1>
+        <div>
+          <h1>Shhh....Going for a short nap to duck better.</h1>
+          <h1>We can keep this a secret, right?</h1>
+        </div>
         <img src="/duck-with-knife.png" />
       </Container>
-      <h2>Till then, you can get to know us here</h2>
-    </React.Fragment>
+      <StyledFooter>
+        <h2>
+          Till then, you can get to know us{' '}
+          <a href="https://workduck.io" rel="noopener noreferrer">
+            here
+          </a>
+        </h2>
+      </StyledFooter>
+    </StyledApp>
   )
 }
 
--- src/Styles/GlobalStyle.ts
+++ src/Styles/GlobalStyle.ts
@@ -6,14 +6,27 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    height: 100%;
-    width: 100vw;
     display: flex;
-    font-family: Inter, sans-serif;
+    align-items: center;
+
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    background-color: #C287E8;
+    color: #0E1428;
+  }
+
+  a {
+    text-decoration: none;
+    color: #FB4D3D;
   }
 
   #root {
     width: 100%;
+    height: 100%;
   }
 `
 
