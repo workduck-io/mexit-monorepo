3eb0deef41c14b376ccb1c488adcf3bb5059b3f6
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-10-05 17:04:11 +0530

    updated filters on reminders view;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Views/Reminders/RemindersAll.tsx
+++ apps/webapp/src/Views/Reminders/RemindersAll.tsx
@@ -8,6 +8,9 @@ import create from 'zustand'
 import { Button } from '@workduck-io/mex-components'
 
 import {
+  Filter,
+  Filters,
+  FilterTypeWithOptions,
   getReminderState,
   mog,
   past,
@@ -22,6 +25,7 @@ import { PageContainer, MainHeader, IntegrationTitle } from '@mexit/shared'
 
 import { useCreateReminderModal } from '../../Components/Reminders/CreateReminderModal'
 import ReminderUI, { reminderStateIcons } from '../../Components/Reminders/Reminder'
+import { reminderFilterFunctions } from '../../Hooks/useFilterFunctions'
 import { FilterStore } from '../../Hooks/useFilters'
 import { useLinks } from '../../Hooks/useLinks'
 import { useReminders } from '../../Hooks/useReminders'
@@ -30,11 +34,11 @@ import SearchFilters from '../SearchFilters'
 import { AllRemindersWrapper, ReminderColumnHeader } from './RemindersAll.style'
 import { ReminderBoardStyled } from './RemindersAll.style'
 
-interface AllReminderFilterStore extends FilterStore<Reminder> {
+interface AllReminderFilterStore extends FilterStore {
   board: ReminderBoard
   setBoard: (board: ReminderBoard) => void
-  addCurrentFilter: (filter: SearchFilter<Reminder>) => void
-  removeCurrentFilter: (filter: SearchFilter<Reminder>) => void
+  addCurrentFilter: (filter: Filter) => void
+  removeCurrentFilter: (filter: Filter) => void
   resetCurrentFilters: () => void
 }
 
@@ -45,6 +49,8 @@ export const useReminderFilter = create<AllReminderFilterStore>((set, get) => ({
   setCurrentFilters: (filters) => set({ currentFilters: filters }),
   filters: [],
   setFilters: (filters) => set({ filters }),
+  globalJoin: 'all',
+  setGlobalJoin: (join) => set({ globalJoin: join }),
   addCurrentFilter: (filter) => {
     set({ currentFilters: [...get().currentFilters, filter] })
   },
@@ -59,15 +65,31 @@ export const useReminderFilter = create<AllReminderFilterStore>((set, get) => ({
 const useReminderFilters = () => {
   const { getPathFromNodeid } = useLinks()
   const { attachBlockData } = useReminders()
+  const setCurrentFilters = useReminderFilter((state) => state.setCurrentFilters)
+  const globalJoin = useReminderFilter((state) => state.globalJoin)
 
-  const getRemindersBoard = (): { board: ReminderBoard; filters: SearchFilter<Reminder>[] } => {
+  const changeCurrentFilter = (filter: Filter) => {
+    const currentFilters = useReminderFilter.getState().currentFilters
+    setCurrentFilters(currentFilters.map((f) => (f.id === filter.id ? filter : f)))
+  }
+
+  const getRemindersBoard = (): { board: ReminderBoard; filters: Filters } => {
     const remindersBase = useReminderStore.getState().reminders
     const currentFilters = useReminderFilter.getState().currentFilters
+
+    // mog('remindersBase', { remindersBase, currentFilters })
 
     const reminders = (
       currentFilters.length > 0
         ? remindersBase.filter((reminder) => {
-            return currentFilters.every((filter) => filter.filter(reminder))
+            return currentFilters.every(
+              (filter) => {
+                // const filterFunction = reminderFilterFunctions[ filter.key ]
+                // mog('filter', { filter, reminder })
+                return reminderFilterFunctions[filter.type](reminder, filter)
+              }
+              // filter.filter(reminder)
+            )
           })
         : remindersBase
     ).map((reminder) => {
@@ -106,7 +128,7 @@ const useReminderFilters = () => {
     }
 
     const newFilters = getReminderFilters([...upcomingReminders, ...pastReminders])
-    mog('NewFilters and Board', { newFilters, upcomingRemindersColumn, pastRemindersColumn })
+    // mog('NewFilters and Board', { newFilters, upcomingRemindersColumn, pastRemindersColumn })
     // setCurrentFilters(newFilters)
     return {
       board: {
@@ -117,7 +139,7 @@ const useReminderFilters = () => {
   }
 
   const getReminderFilters = (reminders: Reminder[]) => {
-    const filters: SearchFilter<Reminder>[] = []
+    const filters: Filters = []
     const nodes = reminders
       .map((reminder) => reminder.nodeid)
       .filter((nodeid, index, self) => self.indexOf(nodeid) === index)
@@ -131,17 +153,21 @@ const useReminderFilters = () => {
       }
       return acc
     }, {} as { [nodeid: string]: number })
-    nodes.forEach((nodeid) => {
-      const path = getPathFromNodeid(nodeid)
-      filters.push({
-        key: 'note',
-        id: nodeid,
-        label: path,
-        filter: (reminder) => reminder.nodeid === nodeid,
-        count: nodeCounts[nodeid],
-        icon: 'ri:file-list-line'
+
+    const nodeFilters: FilterTypeWithOptions = {
+      type: 'note',
+      label: 'Note',
+      options: nodes.map((nodeid) => {
+        const path = getPathFromNodeid(nodeid)
+        return {
+          id: nodeid,
+          label: path,
+          value: nodeid,
+          count: nodeCounts[nodeid]
+        }
       })
-    })
+    }
+    filters.push(nodeFilters)
 
     const allStates = { active: 0, snooze: 0, done: 0, missed: 0 }
 
@@ -150,32 +176,43 @@ const useReminderFilters = () => {
       allStates[status] += 1
     })
 
-    Object.entries(allStates)
-      .filter(([, count]) => count > 0)
-      .forEach(([state, count]) => {
-        filters.push({
+    const stateFilters: FilterTypeWithOptions = {
+      type: 'state',
+      label: 'State',
+      options: Object.entries(allStates)
+        .filter(([, count]) => count > 0)
+        .map(([state, count]) => ({
           key: 'state',
           id: state,
           label: state,
-          icon: reminderStateIcons[state],
+          icon: { type: 'ICON', value: reminderStateIcons[state] },
           count,
-          filter: (reminder: Reminder) => getReminderState(reminder) === state
-        })
-      })
+          value: state
+          // filter: (reminder: Reminder) => getReminderState(reminder) === state
+        }))
+    }
+
+    filters.push(stateFilters)
 
     const todoRemindersLen = reminders.filter((reminder) => reminder.todoid !== undefined).length
     if (todoRemindersLen > 0) {
       filters.push({
-        key: 'has',
-        id: 'block_todo',
-        label: 'Task',
-        icon: 'ri:ri-task-line',
-        count: todoRemindersLen,
-        filter: (reminder: Reminder) => reminder.todoid !== undefined
+        type: 'has',
+        label: 'Has',
+        options: [
+          {
+            id: 'block_todo',
+            label: 'Task',
+            //'ri:ri-task-line',
+            count: todoRemindersLen,
+            value: 'block_todo'
+          }
+        ]
+        // filter: (reminder: Reminder) => reminder.todoid !== undefined
       })
     }
 
-    mog('Filters', { filters })
+    // mog('Filters', { filters })
 
     return filters
   }
@@ -183,13 +220,23 @@ const useReminderFilters = () => {
   const applyFilters = (reminders: Reminder[]) => {
     const currentFilters = useReminderFilter.getState().currentFilters
     return reminders.filter((reminder) => {
-      return currentFilters.every((filter) => filter.filter(reminder))
+      if (globalJoin === 'all') {
+        return currentFilters.every(
+          (filter) => reminderFilterFunctions[filter.type](reminder, filter)
+          // filter.filter(reminder)
+        )
+      } else
+        return currentFilters.some(
+          (filter) => reminderFilterFunctions[filter.type](reminder, filter)
+          // filter.filter(reminder)
+        )
     })
   }
 
   return {
     getReminderFilters,
     getRemindersBoard,
+    changeCurrentFilter,
     applyFilters
   }
 }
@@ -200,41 +247,26 @@ const RemindersAll = () => {
   const addCurrentFilter = useReminderFilter((s) => s.addCurrentFilter)
   const removeCurrentFilter = useReminderFilter((s) => s.removeCurrentFilter)
   const resetCurrentFilters = useReminderFilter((s) => s.resetCurrentFilters)
-  // const filters = useReminderFilter((s) => s.currentFilters)
   const currentFilters = useReminderFilter((s) => s.currentFilters)
   const armedReminders = useReminderStore((s) => s.armedReminders)
-  // const setBoard = useReminderFilter((s) => s.setBoard)
-  // const setFilters = useReminderFilter((s) => s.setFilters)
-  const { getRemindersBoard } = useReminderFilters()
+  const { getRemindersBoard, changeCurrentFilter } = useReminderFilters()
+
+  const globalJoin = useReminderFilter((s) => s.globalJoin)
+  const setGlobalJoin = useReminderFilter((s) => s.setGlobalJoin)
 
   const { board, filters } = useMemo(() => {
     const { board, filters } = getRemindersBoard()
-    // setFilters(newFilters)
-    // mog('Setting Board', { newFilters })
+
     return { board, filters }
   }, [reminders, armedReminders, currentFilters])
 
-  // useEffect(() => {
-  //   const { board, filters } = getRemindersBoard()
-  //   setBoard(board)
-  //   setCurrentFilters(filters)
-  // }, [reminders])
-
   const handleCardMove = (card, source, destination) => {
     mog('card moved', { card, source, destination })
-    // changeStatus(card.todo, destination.toColumnId)
-    // const newReminders = [...reminders]
-    // const cardIndex = newReminders.findIndex((c) => c.id === card.id)
-    // const cardToMove = newReminders[cardIndex]
-    // newReminders.splice(cardIndex, 1)
-    // newReminders.splice(to, 0, cardToMove)
-    // addReminders(newReminders)
   }
 
   const RenderCard = ({ id, reminder }: ReminderBoardCard, { dragging }: { dragging: boolean }) => {
-    // const pC = getPureContent(todo)
     const { getReminderControls } = useReminders()
-    // mog('RenderTodo', { id, todo, dragging })
+
     return (
       <ReminderUI
         showNodeInfo
@@ -266,8 +298,11 @@ const RemindersAll = () => {
           addCurrentFilter={addCurrentFilter}
           removeCurrentFilter={removeCurrentFilter}
           resetCurrentFilters={resetCurrentFilters}
+          changeCurrentFilter={changeCurrentFilter}
           filters={filters}
           currentFilters={currentFilters}
+          globalJoin={globalJoin}
+          setGlobalJoin={setGlobalJoin}
         />
         <ReminderBoardStyled>
           <Board
