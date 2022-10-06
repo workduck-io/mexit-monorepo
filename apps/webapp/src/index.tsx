a8046772f36cef19635d09fcb3f308f1f30e4703
Dinesh Singh <83764394+dineshsingh1@users.noreply.github.com>
2022-10-06 23:27:08 +0530

    Fleet in MexIt (#176)
    https://github.com/workduck-io/mexit-monorepo/pull/176
    
    * Added Fleet with context based section
    
    * AppElement warning for screenreader fixed
    
    * bump @workduck-io/mex-components to v0.0.16
    
    * Changeset and yarn.lock updated

--- apps/webapp/src/index.tsx
+++ apps/webapp/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { createRoot } from 'react-dom/client'
+import Modal from 'react-modal'
 import 'threads/register'
 
 import App from './App'
 
 const root = createRoot(document.getElementById('root'))
+Modal.setAppElement('#root')
 
 root.render(<App />)
