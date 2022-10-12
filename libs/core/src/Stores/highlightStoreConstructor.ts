f21b91e92761e9843247444bf05de5635af52d91
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-10-12 05:52:37 +0530

    removed old shortened links store; highlight store constructor moved to core lib;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

---
+++ libs/core/src/Stores/highlightStoreConstructor.ts
@@ -0,0 +1,81 @@
+import { ILink, SharedNode, Contents, NodeEditorContent } from '../Types/Editor'
+import { mog } from '../Utils/mog'
+import { ElementHighlightMetadata } from '../Utils/serializer'
+
+export interface SingleHighlight {
+  elementMetadata: ElementHighlightMetadata
+  nodeId: string
+  shared?: boolean
+}
+
+export interface SourceHighlights {
+  [blockId: string]: SingleHighlight
+}
+
+export interface Highlighted {
+  [sourceURL: string]: SourceHighlights
+}
+
+export interface HighlightStore {
+  /*
+   * The current ids for specific editors to highlight
+   */
+  highlighted: Highlighted
+  initHighlights: (ilinks: (ILink | SharedNode)[], contents: Contents) => void
+  addHighlightedBlock: (nodeId: string, content: NodeEditorContent) => void
+  clearHighlightedBlock: (url: string, blockId: string) => void
+  clearAllHighlightedBlocks: () => void
+}
+export const highlightStoreConstructor = (set, get) => ({
+  highlighted: {},
+  initHighlights: (ilinks, contents) => {
+    const highlighted = {}
+
+    ilinks.forEach((ilink) => {
+      contents[ilink.nodeid]?.content?.forEach(function (block) {
+        if (block?.metadata?.elementMetadata && this) {
+          highlighted[block.metadata.elementMetadata.sourceUrl] = {
+            ...highlighted[block.metadata.elementMetadata.sourceUrl],
+            [block.id]: {
+              elementMetadata: block.metadata.elementMetadata,
+              nodeId: this.nodeid,
+              shared: !!this?.owner
+            }
+          }
+        }
+      }, ilink)
+    })
+
+    mog('initing highlights', { highlighted })
+    set({ highlighted: highlighted })
+  },
+  addHighlightedBlock: (nodeId, content) => {
+    const { highlighted } = get()
+    const newHighlighted = { ...highlighted }
+
+    content.forEach((item) => {
+      if (item?.metadata?.elementMetadata) {
+        newHighlighted[item.metadata.elementMetadata.sourceUrl] = {
+          ...newHighlighted[item.metadata.elementMetadata.sourceUrl],
+          [item.id]: {
+            elementMetadata: item.metadata.elementMetadata,
+            nodeId
+          }
+        }
+      }
+    })
+    mog('addHighlighted', { newHighlighted })
+    set({ highlighted: newHighlighted })
+  },
+  clearHighlightedBlock: (url, blockId) => {
+    const oldHighlighted = get().highlighted
+    delete oldHighlighted[url][blockId]
+    set({ highlighted: oldHighlighted })
+  },
+  clearAllHighlightedBlocks: () => {
+    const oldHighlighted = get().highlighted
+    const newHighlighted = {}
+    mog('clearAllHighlighted', { oldHighlighted })
+    set({ highlighted: newHighlighted })
+  }
+})
