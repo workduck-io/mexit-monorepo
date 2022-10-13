d6bfc0bf7868984b39b466a4857203f1400df8d6
Dinesh Singh <dinesh@workduck.io>
2022-10-13 12:12:38 +0530

    Namespace check in sidebar

--- apps/webapp/src/Components/Sidebar/PublicSidebar.notes.tsx
+++ apps/webapp/src/Components/Sidebar/PublicSidebar.notes.tsx
@@ -11,22 +11,21 @@ export const PublicNoteSidebar = () => {
   const iLinks = usePublicNodeStore((state) => state.iLinks)
 
   const space: SidebarSpace = useMemo(() => {
-    const namespaceIlinks = iLinks?.filter((item) => item?.namespace === ns?.id)
-
-    return {
-      id: ns.id,
-      label: ns.name,
-      icon: ns.icon ?? {
-        type: 'ICON',
-        value: 'heroicons-outline:view-grid'
-      },
-      tooltip: ns.name,
-      list: {
-        type: 'hierarchy',
-        items: iLinks
-      },
-      pollAction: PollActions.hierarchy
-    } as SidebarSpace
+    if (ns)
+      return {
+        id: ns.id,
+        label: ns.name,
+        icon: ns.icon ?? {
+          type: 'ICON',
+          value: 'heroicons-outline:view-grid'
+        },
+        tooltip: ns.name,
+        list: {
+          type: 'hierarchy',
+          items: iLinks
+        },
+        pollAction: PollActions.hierarchy
+      } as SidebarSpace
   }, [iLinks, ns])
 
   const defaultStyles = { opacity: 1, transform: 'translate3d(0%,0,0)' }
