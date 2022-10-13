fb7fce29bae49bacdf69d4ef968b05b24db232a3
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-10-13 11:13:55 +0530

    fixed sidebar rendering on public namespaces;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Components/Sidebar/SidebarTabs.tsx
+++ apps/webapp/src/Components/Sidebar/SidebarTabs.tsx
@@ -1,5 +1,7 @@
 import { useMatch } from 'react-router-dom'
 
+import { mog } from '@mexit/core'
+
 import { ROUTE_PATHS } from '../../Hooks/useRouting'
 import { useLayoutStore } from '../../Stores/useLayoutStore'
 import ArchiveSidebar from './ArchiveSidebar'
@@ -21,7 +23,7 @@ const SidebarTabs = () => {
   const isTasksView = useMatch(`${ROUTE_PATHS.tasks}/:viewid`)
   const isPublicNamespaceView = useMatch(`${ROUTE_PATHS.namespaceShare}/:namespaceid/*`)
 
-  // mog('IS SIDEBAR', { sidebar, isEditor, isArchive })
+  // mog('IS SIDEBAR', { sidebar, isEditor, isArchive, isPublicNamespaceView })
 
   if (!sidebar.show) return <></>
 
