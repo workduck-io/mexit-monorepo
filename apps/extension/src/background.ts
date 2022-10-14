9f0f1a04821042abd641078d4740197c84250334
Dinesh Singh <dinesh@workduck.io>
2022-10-14 21:37:57 +0530

    On extension update show release notes

--- apps/extension/src/background.ts
+++ apps/extension/src/background.ts
@@ -1,11 +1,12 @@
-import { ActionType, apiURLs, LINK_SHORTENER_URL_BASE, mog, SEPARATOR } from '@mexit/core'
-
-import { handleCaptureRequest, handleActionRequest, handleAsyncActionRequest } from './Utils/requestHandler'
 import * as Sentry from '@sentry/browser'
 import { CaptureConsole } from '@sentry/integrations'
 import fuzzysort from 'fuzzysort'
-import useDataStore from './Stores/useDataStore'
+
+import { ActionType, apiURLs, LINK_SHORTENER_URL_BASE, mog, SEPARATOR } from '@mexit/core'
+
 import { useAuthStore } from './Hooks/useAuth'
+import useDataStore from './Stores/useDataStore'
+import { handleCaptureRequest, handleActionRequest, handleAsyncActionRequest } from './Utils/requestHandler'
 
 Sentry.init({
   dsn: 'https://0c6a334e733d44da96cfd64cc23b1c85@o1127358.ingest.sentry.io/6169172',
@@ -26,11 +27,27 @@ chrome.runtime.onInstalled.addListener((details) => {
       }
     )
   }
+
+  // * On update, show release notes
+  else if (details.reason === 'update') {
+    // * TODO: Use release notes url
+    const url = 'http://localhost:3333/share/namespace/NAMESPACE_88fFQAed8wNJ7jHxdKTa3'
+    chrome.tabs.create(
+      {
+        url,
+        pinned: false
+      },
+      (tab) => {
+        tab.active = true
+      }
+    )
+  }
 })
 
 chrome.commands.onCommand.addListener((command) => {
   chrome.tabs?.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { type: 'sputlit' }, (response) => {
+      console.log('Hey Im sputlit', { response })
       if (!response) {
         chrome.tabs.reload(tabs[0].id)
       }
