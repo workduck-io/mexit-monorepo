d3a5551067f5f665e10877a70d714242237ddd5d
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-10-28 17:46:53 +0530

    persisted layout store;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Stores/useLayoutStore.ts
+++ apps/extension/src/Stores/useLayoutStore.ts
@@ -1,6 +1,9 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
 import { ExtInfobarMode } from '@mexit/shared'
+
+import { asyncLocalStorage } from '../Utils/chromeStorageAdapter'
 
 interface LayoutState {
   rhSidebar: {
@@ -20,27 +23,33 @@ interface LayoutState {
   setInfobarMode: (mode: ExtInfobarMode) => void
 }
 
-export const useLayoutStore = create<LayoutState>((set, get) => ({
-  // RHSidebar
-  rhSidebar: {
-    expanded: false,
-    show: true
-  },
-  toggleTop: 44,
-  setToggleTop: (height) => set({ toggleTop: height }),
-  toggleRHSidebar: () => set((state) => ({ rhSidebar: { ...state.rhSidebar, expanded: !state.rhSidebar.expanded } })),
-  setRHSidebarExpanded: (expanded) => set((state) => ({ rhSidebar: { ...state.rhSidebar, expanded } })),
-  showRHSidebar: () => set((state) => ({ rhSidebar: { ...state.rhSidebar, show: true } })),
-  hideRHSidebar: () => set((state) => ({ rhSidebar: { ...state.rhSidebar, show: false } })),
+export const useLayoutStore = create<LayoutState>(
+  persist(
+    (set, get) => ({
+      // RHSidebar
+      rhSidebar: {
+        expanded: false,
+        show: true
+      },
+      toggleTop: 44,
+      setToggleTop: (height) => set({ toggleTop: height }),
+      toggleRHSidebar: () =>
+        set((state) => ({ rhSidebar: { ...state.rhSidebar, expanded: !state.rhSidebar.expanded } })),
+      setRHSidebarExpanded: (expanded) => set((state) => ({ rhSidebar: { ...state.rhSidebar, expanded } })),
+      showRHSidebar: () => set((state) => ({ rhSidebar: { ...state.rhSidebar, show: true } })),
+      hideRHSidebar: () => set((state) => ({ rhSidebar: { ...state.rhSidebar, show: false } })),
 
-  // Infobar
-  infobar: {
-    visible: true,
-    mode: 'context'
-  },
-  setInfobarMode: (mode) => {
-    const curMode = get().infobar.mode
-    if (curMode === mode) return
-    set({ infobar: { ...get().infobar, mode } })
-  }
-}))
+      // Infobar
+      infobar: {
+        visible: true,
+        mode: 'context'
+      },
+      setInfobarMode: (mode) => {
+        const curMode = get().infobar.mode
+        if (curMode === mode) return
+        set({ infobar: { ...get().infobar, mode } })
+      }
+    }),
+    { name: 'ext-layout-store', getStorage: () => asyncLocalStorage }
+  )
+)
