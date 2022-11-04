0a95806829bb092d8e5bfe048f8a25e255857455
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-11-04 20:36:56 +0530

    used nano id for seed generation;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Utils/generateAvatar.ts
+++ apps/extension/src/Utils/generateAvatar.ts
@@ -1,22 +1,14 @@
-import { createAvatar } from '@dicebear/avatars';
-import * as style from '@dicebear/avatars-male-sprites';
-const generateRandomString = (length) => {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
+import { createAvatar } from '@dicebear/avatars'
+import * as style from '@dicebear/avatars-male-sprites'
+import { nanoid } from 'nanoid'
 
 const generateAvatar = () => {
-    const seed = generateRandomString(Math.floor(Math.random()*10));
-    let svg = createAvatar(style, {
-        seed: seed,
-        // ... and other options
-      });
-    // console.log(svg);
-    return {svg , seed};
+  const seed = nanoid()
+
+  let svg = createAvatar(style, {
+    seed: seed
+  })
+
+  return { svg, seed }
 }
-export default generateAvatar;
+export default generateAvatar
