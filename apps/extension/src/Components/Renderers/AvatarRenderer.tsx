cd3ec8d28e10006f1237cb26972042abbdd7d321
manavagr1108 <manavagr1108@gmail.com>
2022-11-07 14:44:03 +0530

    add download avatar button for avatar generator action

--- apps/extension/src/Components/Renderers/AvatarRenderer.tsx
+++ apps/extension/src/Components/Renderers/AvatarRenderer.tsx
@@ -5,6 +5,8 @@ import styled from 'styled-components'
 
 import { Button } from '@workduck-io/mex-components'
 
+import { mog } from '@mexit/core'
+
 import { useSputlitStore } from '../../Stores/useSputlitStore'
 import { generateAvatar } from '../../Utils/generateAvatar'
 
@@ -23,9 +25,31 @@ const AvatarRenderer = () => {
 
     setScreenshot(imgData)
   }
+  const DownloadAvatar = () => {
+    chrome.runtime.sendMessage(
+      {
+        type: 'DOWNLOAD_AVATAR',
+        data: {
+          url: screenshot
+        }
+      },
+      (response) => {
+        const { message, error } = response
+        if (error) {
+          mog('Avatar Download error', { error })
+        } else {
+          mog('Avatar Downloaded')
+        }
+      }
+    )
+  }
 
   return (
     <Container>
+      <Button onClick={DownloadAvatar}>
+        <Icon icon="pepicons:arrow-down" />
+        Download Avatar
+      </Button>
       <img src={screenshot} />
       <Button onClick={randomizeAvatar}>
         <Icon icon="pepicons:reload" />
