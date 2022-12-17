aa4c2cb0aafde995738835033b18f2ef520fd773
xypnox <xypnox@gmail.com>
2022-12-17 20:05:37 +0530

    Broken extension theming with comments

--- apps/extension/src/index.tsx
+++ apps/extension/src/index.tsx
@@ -1,52 +1,50 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
-import styled, { useTheme } from 'styled-components'
+import styled from 'styled-components'
 
-import { Provider, useThemeContext } from '@workduck-io/mex-themes'
+import { Provider } from '@workduck-io/mex-themes'
 
-import { mog } from '@mexit/core'
 import { Notification } from '@mexit/shared'
 
-import { useUserPreferenceStore } from './Stores/userPreferenceStore'
-import { GlobalStyle } from './Styles/GlobalStyle'
 import Extension from './app'
+import { GlobalStyle } from './Styles/GlobalStyle'
 
-const AutoThemeSwitch = () => {
-  const theme = useUserPreferenceStore((state) => state.theme)
-  const { themes, preferences, changeTheme } = useThemeContext()
-  const curTheme = useTheme()
+// const AutoThemeSwitch = () => {
+//   const theme = useUserPreferenceStore((state) => state.theme)
+//   const { themes, preferences, changeTheme } = useThemeContext()
+//   const curTheme = useTheme()
 
-  mog('theme', { theme, preferences, curTheme })
+//   mog('theme', { theme, preferences, curTheme })
 
-  useEffect(() => {
-    changeTheme(preferences.themeId)
-  }, [])
+//   useEffect(() => {
+//     changeTheme(preferences.themeId)
+//   }, [])
 
-  useEffect(() => {
-    if (theme) {
-      if (theme !== preferences.themeId) {
-        mog('theme', { theme, preferences })
-        changeTheme(theme)
-      }
-    }
-  }, [theme])
+//   useEffect(() => {
+//     if (theme) {
+//       if (theme !== preferences.themeId) {
+//         mog('theme', { theme, preferences })
+//         changeTheme(theme)
+//       }
+//     }
+//   }, [theme])
 
-  return null
-}
+//   return null
+// }
 
 const SimpleCompo = styled.div`
   background: ${({ theme }) => {
-    console.log({ theme })
+    console.log('Theme inside our styled component', { theme })
     return theme.tokens.surfaces.app
   }};
 `
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // <AutoThemeSwitch />
   return (
     <Provider legacySupport={false}>
       <div>Simple</div>
       <SimpleCompo>What is this</SimpleCompo>
-      <AutoThemeSwitch />
       {children}
     </Provider>
   )
