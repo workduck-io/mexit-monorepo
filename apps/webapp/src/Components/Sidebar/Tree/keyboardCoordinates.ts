5a5493c783fcb53947652c68b728ca00d4c8072c
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-12-17 15:08:01 +0530

    fixed webapp build;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Components/Sidebar/Tree/keyboardCoordinates.ts
+++ apps/webapp/src/Components/Sidebar/Tree/keyboardCoordinates.ts
@@ -3,20 +3,15 @@ import {
   DroppableContainer,
   getFirstCollision,
   KeyboardCode,
-  KeyboardCoordinateGetter,
-} from '@dnd-kit/core';
+  KeyboardCoordinateGetter
+} from '@dnd-kit/core'
 
-import type {SensorContext} from './types';
-import {getProjection} from './utilities';
+import type { SensorContext } from './types'
+import { getProjection } from './utilities'
 
-const directions: string[] = [
-  KeyboardCode.Down,
-  KeyboardCode.Right,
-  KeyboardCode.Up,
-  KeyboardCode.Left,
-];
+const directions: string[] = [KeyboardCode.Down, KeyboardCode.Right, KeyboardCode.Up, KeyboardCode.Left]
 
-const horizontal: string[] = [KeyboardCode.Left, KeyboardCode.Right];
+const horizontal: string[] = [KeyboardCode.Left, KeyboardCode.Right]
 
 export const sortableTreeKeyboardCoordinates: (
   context: SensorContext,
@@ -24,136 +19,122 @@ export const sortableTreeKeyboardCoordinates: (
   indentationWidth: number
 ) => KeyboardCoordinateGetter =
   (context, indicator, indentationWidth) =>
-  (
-    event,
-    {
-      currentCoordinates,
-      context: {
-        active,
-        over,
-        collisionRect,
-        droppableRects,
-        droppableContainers,
-      },
-    }
-  ) => {
+  (event, { currentCoordinates, context: { active, over, collisionRect, droppableRects, droppableContainers } }) => {
     if (directions.includes(event.code)) {
       if (!active || !collisionRect) {
-        return;
+        return
       }
 
-      event.preventDefault();
+      event.preventDefault()
 
       const {
-        current: {items, offset},
-      } = context;
+        current: { items, offset }
+      } = context
 
       if (horizontal.includes(event.code) && over?.id) {
-        const {depth, maxDepth, minDepth} = getProjection(
+        const { depth, maxDepth, minDepth } = getProjection(
           items,
-          active.id,
-          over.id,
+          active.id as string,
+          over.id as string,
           offset,
           indentationWidth
-        );
+        )
 
         switch (event.code) {
           case KeyboardCode.Left:
             if (depth > minDepth) {
               return {
                 ...currentCoordinates,
-                x: currentCoordinates.x - indentationWidth,
-              };
+                x: currentCoordinates.x - indentationWidth
+              }
             }
-            break;
+            break
           case KeyboardCode.Right:
             if (depth < maxDepth) {
               return {
                 ...currentCoordinates,
-                x: currentCoordinates.x + indentationWidth,
-              };
+                x: currentCoordinates.x + indentationWidth
+              }
             }
-            break;
+            break
         }
 
-        return undefined;
+        return undefined
       }
 
-      const containers: DroppableContainer[] = [];
+      const containers: DroppableContainer[] = []
 
       droppableContainers.forEach((container) => {
         if (container?.disabled || container.id === over?.id) {
-          return;
+          return
         }
 
-        const rect = droppableRects.get(container.id);
+        const rect = droppableRects.get(container.id)
 
         if (!rect) {
-          return;
+          return
         }
 
         switch (event.code) {
           case KeyboardCode.Down:
             if (collisionRect.top < rect.top) {
-              containers.push(container);
+              containers.push(container)
             }
-            break;
+            break
           case KeyboardCode.Up:
             if (collisionRect.top > rect.top) {
-              containers.push(container);
+              containers.push(container)
             }
-            break;
+            break
         }
-      });
+      })
 
       const collisions = closestCorners({
         active,
         collisionRect,
         pointerCoordinates: null,
         droppableRects,
-        droppableContainers: containers,
-      });
-      let closestId = getFirstCollision(collisions, 'id');
+        droppableContainers: containers
+      })
+      let closestId = getFirstCollision(collisions, 'id')
 
       if (closestId === over?.id && collisions.length > 1) {
-        closestId = collisions[1].id;
+        closestId = collisions[1].id
       }
 
       if (closestId && over?.id) {
-        const activeRect = droppableRects.get(active.id);
-        const newRect = droppableRects.get(closestId);
-        const newDroppable = droppableContainers.get(closestId);
+        const activeRect = droppableRects.get(active.id)
+        const newRect = droppableRects.get(closestId)
+        const newDroppable = droppableContainers.get(closestId)
 
         if (activeRect && newRect && newDroppable) {
-          const newIndex = items.findIndex(({id}) => id === closestId);
-          const newItem = items[newIndex];
-          const activeIndex = items.findIndex(({id}) => id === active.id);
-          const activeItem = items[activeIndex];
+          const newIndex = items.findIndex(({ id }) => id === closestId)
+          const newItem = items[newIndex]
+          const activeIndex = items.findIndex(({ id }) => id === active.id)
+          const activeItem = items[activeIndex]
 
           if (newItem && activeItem) {
-            const {depth} = getProjection(
+            const { depth } = getProjection(
               items,
-              active.id,
-              closestId,
+              active.id as string,
+              closestId as string,
               (newItem.depth - activeItem.depth) * indentationWidth,
               indentationWidth
-            );
-            const isBelow = newIndex > activeIndex;
-            const modifier = isBelow ? 1 : -1;
-            const offset = indicator
-              ? (collisionRect.height - activeRect.height) / 2
-              : 0;
+            )
+            const isBelow = newIndex > activeIndex
+            const modifier = isBelow ? 1 : -1
+            const offset = indicator ? (collisionRect.height - activeRect.height) / 2 : 0
 
             const newCoordinates = {
               x: newRect.left + depth * indentationWidth,
-              y: newRect.top + modifier * offset,
-            };
+              y: newRect.top + modifier * offset
+            }
 
-            return newCoordinates;
+            return newCoordinates
           }
         }
       }
     }
 
-    return undefined;
-  };
+    return undefined
+  }
