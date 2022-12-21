fec9fe82a385b21b229fb17d1767eb8f5bd2d870
xypnox <xypnox@gmail.com>
2022-12-21 19:33:08 +0530

    Fix error tooltip colors in auth forms

--- apps/webapp/src/Style/Tippy.tsx
+++ apps/webapp/src/Style/Tippy.tsx
@@ -1,21 +1,20 @@
-import { transparentize } from 'polished'
 import styled from 'styled-components'
 
 import { CardShadow } from '@mexit/shared'
 
 export const TooltipBase = styled.div`
   padding: ${({ theme }) => theme.spacing.small};
-  background: ${({ theme }) => transparentize(0.5, theme.colors.gray[8])} !important;
+  background: rgba(${({ theme }) => theme.rgbTokens.surfaces.s[1]}, 0.5);
 
   backdrop-filter: blur(8px);
 
   border-radius: ${({ theme }) => theme.borderRadius.small};
-  color: ${({ theme }) => theme.colors.text.fade};
+  color: ${({ theme }) => theme.tokens.text.fade};
   ${CardShadow}
 `
 
 export const Tooltip = styled(TooltipBase)`
-  background: ${({ theme }) => transparentize(0.5, theme.colors.gray[9])} !important;
+  background: ${({ theme }) => theme.tokens.surfaces.s[2]} !important;
   max-height: 400px;
   max-width: 700px;
   overflow-y: auto;
@@ -23,9 +22,9 @@ export const Tooltip = styled(TooltipBase)`
 
 export const ErrorTooltip = styled.div`
   padding: ${({ theme }) => theme.spacing.tiny};
-  background: ${({ theme }) => transparentize(0.3, theme.colors.gray[8])} !important;
+  background: ${({ theme }) => theme.tokens.surfaces.s[3]};
   backdrop-filter: blur(8px);
-  color: ${({ theme }) => theme.colors.text.fade};
+  color: ${({ theme }) => theme.tokens.text.fade};
   border-radius: ${({ theme }) => theme.borderRadius.small};
   font-weight: 400;
   font-size: 0.9rem;
