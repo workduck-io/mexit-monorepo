570b117f2304b6adca1e5aaf590022dcbe1d24c2
Mukul Mehta <mukul@workduck.io>
2023-02-22 16:35:55 +0530

    Fix build type issues with fuzzySearch in webapp

--- libs/core/src/Utils/fuzzysearch.ts
+++ libs/core/src/Utils/fuzzysearch.ts
@@ -1,12 +1,9 @@
-import { search } from 'fast-fuzzy'
+import { type FullOptions, search } from 'fast-fuzzy'
 
 type FuzzySearchable = string | object
+type KeySelector<T extends FuzzySearchable> = (item: T) => string | string[]
 
-export const fuzzySearch = <T extends FuzzySearchable>(
-  list: Array<T>,
-  text: string,
-  keySelector = (item: T): FuzzySearchable => item
-) => {
-  const results = search<T, any>(text, list, { keySelector: keySelector })
+export const fuzzySearch = <T extends FuzzySearchable>(list: Array<T>, text: string, keySelector?: KeySelector<T>) => {
+  const results = search<T, FullOptions<T>>(text, list, { keySelector: keySelector, returnMatchData: false })
   return results
 }
