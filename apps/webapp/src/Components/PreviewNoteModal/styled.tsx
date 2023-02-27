091e13648c35285a72df988a503f3ae9ad7682fe
Dinesh Singh <dinesh@workduck.io>
2023-02-27 21:55:11 +0530

    On Blur clear buffer, Modal UI changes

--- apps/webapp/src/Components/PreviewNoteModal/styled.tsx
+++ apps/webapp/src/Components/PreviewNoteModal/styled.tsx
@@ -7,12 +7,13 @@ export const PreviewNoteContainer = styled.section`
   height: 50vh;
   max-height: 50vh;
   max-width: 50vw;
-  
+
   display: flex;
   flex-direction: column;
   align-items: center;
 
   ${EditorStyles} {
-    /* background: ${({ theme }) => theme.colors.background.app}; */
+    max-height: calc(50vh - 4rem);
+    overflow: hidden auto;
   }
 `
--- apps/webapp/src/Hooks/useOnUnload.ts
+++ apps/webapp/src/Hooks/useOnUnload.ts
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 
+import { mog } from '@mexit/core'
+
 import { useAuthStore } from '../Stores/useAuth'
 
 import { useEditorBuffer, useSnippetBuffer } from './useEditorBuffer'
@@ -12,13 +14,18 @@ export const useOnUnload = () => {
   useEffect(() => {
     if (authenticated) {
       const onUnload = () => {
+        mog('Saving notes and snippets...')
         clearNotesBuffer()
         clearSnippetsBuffer()
       }
 
       window.addEventListener('beforeunload', onUnload)
+      window.addEventListener('blur', onUnload)
 
-      return () => window.removeEventListener('beforeunload', onUnload)
+      return () => {
+        window.removeEventListener('beforeunload', onUnload)
+        window.removeEventListener('blur', onUnload)
+      }
     }
   }, [authenticated])
 }
