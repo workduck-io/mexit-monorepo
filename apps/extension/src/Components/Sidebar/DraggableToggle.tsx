38f2c7d2d355de6842b6c85f2bdd22538a62c825
Sahil Shubham <sahil.shubham2000@gmail.com>
2023-06-05 21:14:35 +0530

    added button animation behind drag toggle;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/extension/src/Components/Sidebar/DraggableToggle.tsx
+++ apps/extension/src/Components/Sidebar/DraggableToggle.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { animated, useSpring } from 'react-spring'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useSpring, useSprings } from 'react-spring'
 
-import { Icon } from '@iconify/react'
 import Tippy from '@tippyjs/react'
 import { useGesture } from '@use-gesture/react'
-import styled, { css } from 'styled-components'
 
 import { TitleWithShortcut } from '@workduck-io/mex-components'
 
@@ -13,69 +11,7 @@ import { WDLogo } from '@mexit/shared'
 
 import { getElementById } from '../../Utils/cs-utils'
 
-const DragIcon = styled(Icon)<{ $show: boolean }>`
-  margin-right: -18px;
-  opacity: 0;
-  pointer-events: none;
-  transition: margin-right 0.2s ease-in-out, opacity 0.2s ease-in-out;
-  ${(props) =>
-    props.$show &&
-    css`
-      margin-right: 0;
-      opacity: 1;
-      pointer-events: all;
-    `}
-`
-
-const ToggleWrapper = styled(animated.div)`
-  display: flex;
-  align-items: center;
-  width: max-content;
-  position: relative;
-
-  z-index: 9999999999;
-  padding: 8px;
-  border-radius: ${({ theme }) => theme.borderRadius.small};
-  background: ${({ theme }) => theme.tokens.surfaces.sidebar};
-  color: ${({ theme }) => theme.tokens.text.fade};
-  transition: right 0.2s ease-in-out, background 0.2s ease-in-out, width 0.2s ease-in-out;
-
-  svg {
-    height: 16px;
-    width: 16px;
-  }
-
-  &:hover,
-  &:active {
-    cursor: pointer;
-    box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.25);
-    background: ${({ theme }) => theme.tokens.colors.primary.default};
-    color: ${({ theme }) => theme.tokens.colors.primary.text};
-
-    svg {
-      path {
-        fill: ${({ theme }) => theme.tokens.surfaces.sidebar};
-      }
-    }
-  }
-
-  ${DragIcon} {
-    cursor: ns-resize;
-  }
-
-  &:active {
-    transition: background 0.1s ease;
-    background-color: ${({ theme }) => theme.tokens.colors.primary.default};
-    color: ${({ theme }) => theme.tokens.colors.primary.text};
-  }
-`
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-`
+import { Circle, DragIcon, ToggleWrapper, Wrapper } from './styled'
 
 export const DraggableToggle = () => {
   const [isHovering, setIsHovering] = useState(false)
@@ -84,9 +20,13 @@ export const DraggableToggle = () => {
   // TODO: Not using this for now, was having issues with calc() inside api.start()
   // const { endColumnWidth } = useSidebarTransition()
 
-  const handleRef = useRef<any>(null)
+  const avatarRefs = useRef<HTMLDivElement[]>([])
+  const avatarRefInitialPositions = useRef<number[]>([])
+  const toggleRef = useRef<HTMLDivElement>(null)
+  const avatarTimeoutRef = useRef<ReturnType<typeof setTimeout>>()
 
   const [{ x, y }, api] = useSpring(() => ({ x: '97vw', y: toggleTop }), [])
+  const [buttonSprings, buttonApi] = useSprings(3, (i) => ({ y: 0 }), [])
 
   const bind = useGesture(
     {
@@ -98,6 +38,22 @@ export const DraggableToggle = () => {
       },
       onHover: ({ hovering }) => {
         setIsHovering(hovering)
+
+        if (hovering) {
+          if (avatarTimeoutRef.current) {
+            clearTimeout(avatarTimeoutRef.current)
+          }
+
+          buttonApi.start({
+            y: 0
+          })
+        } else {
+          avatarTimeoutRef.current = setTimeout(() => {
+            buttonApi.start((i) => ({
+              y: avatarRefInitialPositions.current[i]
+            }))
+          }, 1500)
+        }
       },
       onClick: () => {
         toggleRHSidebar()
@@ -116,27 +72,40 @@ export const DraggableToggle = () => {
     }
   )
 
+  useLayoutEffect(() => {
+    if (avatarRefInitialPositions.current.length === 0) {
+      const { y: buttonY } = toggleRef.current.getBoundingClientRect()
+
+      avatarRefInitialPositions.current = avatarRefs.current.map((node) => buttonY - node.getBoundingClientRect().y)
+    }
+
+    buttonApi.start((i) => ({
+      y: avatarRefInitialPositions.current[i],
+      immediate: true
+    }))
+  }, [])
+
   useEffect(() => {
-    api.start({ x: rhSidebar.expanded ? '63vw' : '97vw' })
+    api.start({ x: rhSidebar.expanded ? '62vw' : '96vw' })
   }, [rhSidebar.expanded])
 
-  useEffect(() => {
-    console.log('vertical', { y, toggleTop })
-  }, [y, toggleTop])
-
   return (
-    <Tippy
-      theme="mex-bright"
-      placement="left"
-      appendTo={() => getElementById('ext-side-nav')}
-      content={<TitleWithShortcut title={rhSidebar.expanded ? 'Collapse Sidebar' : 'Expand Sidebar'} />}
-    >
-      <ToggleWrapper {...bind()} style={{ x, y }}>
+    <ToggleWrapper ref={toggleRef} {...bind()} style={{ x, y }}>
+      <Tippy
+        theme="mex-bright"
+        placement="left"
+        appendTo={() => getElementById('ext-side-nav')}
+        content={<TitleWithShortcut title={rhSidebar.expanded ? 'Collapse Sidebar' : 'Expand Sidebar'} />}
+      >
         <Wrapper>
           <WDLogo />
-          <DragIcon ref={handleRef} $show={isHovering} icon="ic:outline-drag-indicator" />
+          <DragIcon $show={isHovering} icon="ic:outline-drag-indicator" />
         </Wrapper>
-      </ToggleWrapper>
-    </Tippy>
+      </Tippy>
+
+      {buttonSprings.map((springs, index) => (
+        <Circle key={index} ref={(ref) => (avatarRefs.current[index] = ref!)} style={springs} />
+      ))}
+    </ToggleWrapper>
   )
 }
