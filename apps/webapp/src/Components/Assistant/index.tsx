593ae896668cf00d37fb07b87ec9a96df4ee5d2d
Sahil Shubham <sahil.shubham2000@gmail.com>
2023-06-14 20:51:26 +0530

    collapsible assistant;
    Signed-off-by: Sahil Shubham <sahil.shubham2000@gmail.com>

--- apps/webapp/src/Components/Assistant/index.tsx
+++ apps/webapp/src/Components/Assistant/index.tsx
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 
+import arrowLeftSLine from '@iconify/icons-ri/arrow-left-s-line'
 import { getPlateEditorRef } from '@udecode/plate'
+import { useTheme } from 'styled-components'
 
 import { SupportedAIEventTypes, useEditorStore, useHistoryStore } from '@mexit/core'
-import { AutoComplete, useAIOptions } from '@mexit/shared'
+import { AutoComplete, DefaultMIcons, Group, IconDisplay, useAIOptions } from '@mexit/shared'
+
+import { AccordionContent, Chevron, GroupHeader } from '../Views/ViewBlockRenderer/BlockContainer'
 
 import { MessageRenderer } from './MessageRenderer'
+import { ConversationContainer } from './styled'
 
 const Assistant = () => {
+  const [isOpen, setIsOpen] = useState(true)
+  const theme = useTheme()
   const node = useEditorStore((state) => state.node)
   const { performAIAction } = useAIOptions()
   const aiHistory = useHistoryStore((store) => store.ai)
@@ -23,19 +30,26 @@ const Assistant = () => {
     }
   }
 
-  useEffect(() => {
-    console.log(aiHistory)
-  }, [aiHistory])
+  const handleToggleAccordion = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
-    // Chat Wrapper
-    <div>
-      <MessageRenderer conversationStack={aiHistory} />
+    <ConversationContainer>
+      <GroupHeader padding isOpen={!!isOpen} onClick={handleToggleAccordion}>
+        <Group>
+          <IconDisplay icon={DefaultMIcons.AI} color={theme.tokens.colors.primary.hover} />
+          <span>AI Assistant</span>
+        </Group>
+        <Chevron isOpen={isOpen} onClick={handleToggleAccordion} height={24} width={24} icon={arrowLeftSLine} />
+      </GroupHeader>
 
-      <div>
+      <AccordionContent isOpen={isOpen}>
+        <MessageRenderer conversationStack={aiHistory} />
+
         <AutoComplete onEnter={handleOnEnter} clearOnEnter={true} defaultItems={[]} />
-      </div>
-    </div>
+      </AccordionContent>
+    </ConversationContainer>
   )
 }
 
--- apps/webapp/src/Components/Assistant/styled.ts
+++ apps/webapp/src/Components/Assistant/styled.ts
@@ -4,7 +4,7 @@ const MessageBubble = styled.div`
   padding: ${({ theme }) => theme.spacing.small};
   margin: ${({ theme }) => theme.spacing.small};
 
-  background-color: ${({ theme }) => theme.tokens.surfaces.app};
+  background-color: ${({ theme }) => theme.tokens.surfaces.modal};
   border-radius: ${({ theme }) => theme.borderRadius.tiny};
 `
 
@@ -24,4 +24,16 @@ export const ConversationWrapper = styled.div`
   margin: ${({ theme }) => theme.spacing.small} 0;
   height: 20rem;
   overflow-y: scroll;
+`
+
+export const ConversationContainer = styled.div`
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  background: ${({ theme }) => `rgba(${theme.rgbTokens.surfaces.s[3]}, 0.4)`};
+  transition: all 0.2s ease-in;
+
+  :hover {
+    background: ${({ theme }) => `rgba(${theme.rgbTokens.surfaces.s[4]}, 0.3)`};
+  }
+  backdrop-filter: blur(10px);
+  padding: ${({ theme }) => theme.spacing.small};
 `
